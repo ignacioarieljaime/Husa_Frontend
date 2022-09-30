6267f1282ca3811ad9b142c076920ca80c45b697
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-09-30 10:17:12 +0330

    fix build command

--- src/config/RemovePages.js
+++ src/config/RemovePages.js
@@ -1,13 +1,13 @@
 const path = require('path')
 const fs = require('fs')
 
-fs.readdir(path.resolve(__dirname, 'pages'), (err, files) => {
+fs.readdir(path.resolve(__dirname, '../pages'), (err, files) => {
 	if (err) throw err
 	if (files.includes('index.js')) {
 		files.forEach(file => {
 			if (file !== '404.js' && file !== '_app.js') {
 				fs.rm(
-					path.resolve(__dirname, 'pages', file),
+					path.resolve(__dirname, '../pages', file),
 					{ recursive: true, force: true },
 					err => {
 						if (err) {
