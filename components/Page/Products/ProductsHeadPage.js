e81761dcd15a7785725cb67df30be50457585b6b
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-05 15:42:13 +0330

    fix productsHead component

--- components/Page/Products/ProductsHeadPage.js
+++ components/Page/Products/ProductsHeadPage.js
@@ -3,10 +3,14 @@ import React from 'react'
 // image
 import TvImage from 'public/assets/images/all-tvs/all-tvs-header.png'
 
-function ProductsHeadPage() {
+function ProductsHeadPage({ data: { props } }) {
 	return (
 		<div className='all-tvs-heading'>
-			<img src={TvImage.src} alt='smart tv platforms' className='col-12 col-md-6' />
+			<img
+				src={props.data}
+				alt='smart tv platforms'
+				className='col-12 col-md-6'
+			/>
 		</div>
 	)
 }
