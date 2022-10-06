81616a6b039f48b02264b6b0f100a12a443a022a
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-06 15:24:21 +0330

    reset controller

---
+++ controller/PageController.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+module.exports = async (page, content) => {
+	if (page.isHome) {
+		fs.writeFile(
+			`${page.pageRoute}/${page.dynamicRote ? page.dynamicRote : 'index.js'}`,
+			content,
+			err => {
+				if (err) {
+					console.error(err)
+				}
+			}
+		)
+	}
+	if (!fs.existsSync(page.pageRoute)) {
+		await fs.mkdirSync(page.pageRoute)
+		fs.writeFile(
+			`${page.pageRoute}/${page.dynamicRote ? page.dynamicRote : 'index.js'}`,
+			content,
+			err => {
+				if (err) {
+					console.error(err)
+				}
+			}
+		)
+	}
+}
