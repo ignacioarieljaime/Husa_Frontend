78f907d6ba837086e92a6fadcd9d38567071edc5
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-08 11:33:41 +0330

    fix

--- components/Page/Landing/LandingVideoPlayer.js
+++ components/Page/Landing/LandingVideoPlayer.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-function LandingVideoPlayer({ data: { props } }) {
+function LandingVideoPlayer({
+	data: {
+		structure: { video }
+	}
+}) {
 	return (
-		<div className='video-container landing'>
+		<div om className='video-container landing'>
 			<iframe
-				title='TV advertisement'
-				// src={JSON.parse(props).data}
+				src={video.src}
+				alt={video.alt}
+				title={video.title}
 				width='100%'
 				height='100%'
 				frameborder='0'
