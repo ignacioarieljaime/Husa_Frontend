6b54451262864aaa51b1f1b373218e10d538d85c
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-10 12:15:59 +0330

    wip

--- components/Page/Products/ProductGrid.js
+++ components/Page/Products/ProductGrid.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Spinner from 'components/common/Spinner'
 import { useRouter } from 'next/router'
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { GetProductByFilterApi } from 'services/Product'
 
 // components
