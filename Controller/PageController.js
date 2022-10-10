19beff35dc3346dcdf7addb0e9f368d740cdef55
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-10 17:38:23 +0330

    fix pageRemover and pageController

--- Controller/PageController.js
+++ Controller/PageController.js
@@ -2,11 +2,15 @@ const fs = require('fs')
 
 module.exports = async (page, content) => {
 	let route = ''
+	console.log(page.route ,'---------------')
 	if (page.route.includes('/index')) {
 		route = page.route.replace('/index', '')
 	} else {
-		route = page.route
+		page.route[0] === '/'
+			? (route = page.route.replace('/', ''))
+			: (route = page.route)
 	}
+	console.log(route ,"++++++++++++++++++++++ route");
 	if (route === '/') {
 		fs.writeFile(`./pages/index.js`, content, err => {
 			if (err) {
--- config/RemovePages.js
+++ config/RemovePages.js
@@ -3,20 +3,18 @@ const fs = require('fs')
 
 fs.readdir(path.resolve(__dirname, '../pages'), (err, files) => {
 	if (err) throw err
-	if (files.includes('index.js')) {
-		files.forEach(file => {
-			if (file !== '404.js' && file !== '_app.js') {
-				fs.rm(
-					path.resolve(__dirname, '../pages', file),
-					{ recursive: true, force: true },
-					err => {
-						if (err) {
-							return console.log('error occurred in deleting directory', err)
-						}
+	files.forEach(file => {
+		if (file !== '404.js' && file !== '_app.js') {
+			fs.rm(
+				path.resolve(__dirname, '../pages', file),
+				{ recursive: true, force: true },
+				err => {
+					if (err) {
+						return console.log('error occurred in deleting directory', err)
 					}
-				)
-			}
-		})
-		console.log('Directory deleted successfully')
-	}
+				}
+			)
+		}
+	})
+	console.log('Directory deleted successfully')
 })
