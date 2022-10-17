85c0e542f26a0b8a75c87c65c9f252eb8c7c6f94
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-17 13:36:46 +0330

    fix page

--- Controller/PageGeneratorController.js
+++ Controller/PageGeneratorController.js
@@ -25,18 +25,19 @@ function Index({data}) {
 		<Layout>
 		<section> ${ComponentList.map(
 			componentItem =>
-				`{components.includes('${componentItem.name}') && <${
-					componentItem.name
-				} data={data.widgets.find(item => item.name === '${componentItem.name}')}/>}`
+				`{components.includes('${componentItem.name}') && <${componentItem.name} data={data.widgets.find(item => item.name === '${componentItem.name}')}/>}`
 		).join('')}</section>
 		</Layout>
 			
 	)
 }
 	
-export async function getServerSideProps() {
+export async function getServerSideProps(context) {
+	console.log(context?.query?.param)
 	console.log('send ssr request')
-	let data = await axios
+	${
+		_page.model_id === 0 && _page.model_type
+			? `	let data = await axios
 		.get(
 			'https://imcxm.dev-api.hisenseportal.com/api/husa/getPageInfo/${_page.id}'
 		)
@@ -48,8 +49,27 @@ export async function getServerSideProps() {
 			console.error('Error:', error)
 			return null
 		})
+		return { props: { data } }
+		`
+			: `let data = await axios.get(
+			'https://imcxm.dev-api.hisenseportal.com/api/husa/getDynamicPages/${_page.model_id}'
+		)
+		.then(response => {
+			console.log('get ssr data')
+			return response.data.data[0]
+		})
+		.catch(error => {
+			console.error('Error:', error)
+			return null
+		});
+		return { props: { data } }
+		
+		
+		`
+	}
 
-	return { props: { data } }
+
+	
 }
 export default Index`
 }
