e56daabdf3eee7410b8e2d25b0a8dfc1f23bcd06
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-20 14:52:43 +0330

    fix subscribe

--- components/common/Subscribe.js
+++ components/common/Subscribe.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 function Subscribe() {
+	const [emailName, setEmailName] = useState()
+
+	const sendEmail = async e => {
+		e.preventDefault();
+		try {
+			let response = await axios.post(
+				'https://imcrm.dev-api.hisenseportal.com/api/hisense/lead',
+				{ email: emailName }
+			)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+	
 	return (
 		<div className='container-fluid email-form px-8 py-14 px-md-12 p-md-20'>
 			<article className='row p-lg-8'>
@@ -9,13 +24,16 @@ function Subscribe() {
 					<br />
 					about new products & other news
 				</h2>
-				<form className='col-12 col-md-6 mx-0 my-0 my-md-auto'>
+				<form
+					onSubmit={sendEmail}
+					className='col-12 col-md-6 mx-0 my-0 my-md-auto'>
 					<input
 						className='form-control'
+						onChange={e => setEmailName(e.target.value)}
 						type='email'
 						placeholder='Enter your email here'
 					/>
-					<button>Subscribe</button>
+					<button type='submit'>Subscribe</button>
 				</form>
 			</article>
 		</div>
