56478f4c3a07fb21b0136fdde05c31078179aab6
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-20 14:52:08 +0330

    fix need help box

--- components/common/NeedHelpBox.js
+++ components/common/NeedHelpBox.js
@@ -1,60 +1,22 @@
 import React from 'react'
 
-function NeedHelpBox() {
+function NeedHelpBox({ data: { structure } }) {
 	return (
 		<section>
 			<div className='support-help'>
 				<h3>Need More Help?</h3>
 				<div className='container'>
 					<div className='row justify-content-evenly align-items-center'>
-						<div className='help-box'>
-							<h4>REGISTER PRODUCT</h4>
-							<a
-								href='/pages/support/register/index.html'
-								className='btn btn-secondary btn-hover-primary'>
-								Get Started
-							</a>
-						</div>
-						<div className='help-box'>
-							<h4>FAQ’s</h4>
-							<a
-								href='/pages/support/faq/index.html'
-								className='btn btn-secondary btn-hover-primary'>
-								find out more
-							</a>
-						</div>
-						<div className='help-box'>
-							<h4>videos</h4>
-							<a
-								href='/pages/support/video-tutorials/index.html'
-								className='btn btn-secondary btn-hover-primary'>
-								Watch Now
-							</a>
-						</div>
-						<div className='help-box'>
-							<h4>replacement parts</h4>
-							<a
-								href='https://hisense.encompass.com/'
-								className='btn btn-secondary btn-hover-primary'>
-								order now
-							</a>
-						</div>
-						<div className='help-box'>
-							<h4>contact hisense</h4>
-							<a
-								href='/pages/contact/index.html'
-								className='btn btn-secondary btn-hover-primary'>
-								order now
-							</a>
-						</div>
-						<div className='help-box'>
-							<h4>Technical support</h4>
-							<a
-								href='/pages/contact/index.html'
-								className='btn btn-secondary btn-hover-primary'>
-								contact
-							</a>
-						</div>
+						{structure.list.value.map((item, index) => (
+							<div className='help-box'>
+								<h4>{item.title.value}</h4>
+								<a
+									href={item.url.value}
+									className='btn btn-secondary btn-hover-primary'>
+									Get Started
+								</a>
+							</div>
+						))}
 					</div>
 				</div>
 			</div>
