7cb31213e83489ce5aecccb6c01b4e2a9bcb1e24
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-25 12:33:00 +0330

    fix new home page

--- components/page/HomePageMB/HomePageMBDiscoveryItem.js
+++ components/page/HomePageMB/HomePageMBDiscoveryItem.js
@@ -7,7 +7,12 @@ const HomePageMBDiscoveryItem = ({ image, title, paragraph, link, width }) => {
 		<div
 			className={'home-page-mb-discovery-item py-10 px-6 px-md-13 py-md-15'}
 			style={{ width: width + '%' }}>
-			<img src={image.src} alt={image.alt} width='100%' className='mb-7' />
+			<img
+				height={'100px'}
+				src={image.src}
+				alt={image.alt}
+				className='mb-7'
+			/>
 			<h3 className='fs-base mb-7'>{title}</h3>
 			<p className='fs-base mb-9'>{paragraph}</p>
 			<a href={link.value} className='n-btn primary-text'>
