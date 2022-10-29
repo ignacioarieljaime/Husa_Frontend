47ce694a6533ff27b1a167e116e020a4dea15408
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-29 14:31:24 +0330

    fix lets-get-real page

--- components/page/ULED/ULEDProductItem.js
+++ components/page/ULED/ULEDProductItem.js
@@ -15,8 +15,8 @@ function ULEDProductItem({ data }) {
 			<div className='col-12 col-lg-4 platform platform-uled'>
 				<h2>{title?.value}</h2>
 				<p className='text-black fs-2 fw-normal'>{description?.value}</p>
-				<div className='py-9'>
-					<img src={image?.src} alt={image?.alt} width='100%' height='100%' />
+				<div className='py-9 text-center'>
+					<img src={image?.src} alt={image?.alt} height='300px' />
 				</div>
 				<div className='row align-items-center'>
 					<div className='col-12 col-sm-3 col-lg-6 col-xl-4  gap-3 mb-5 mb-sm-0'>
@@ -24,16 +24,18 @@ function ULEDProductItem({ data }) {
 							<a className='btn btn-light text-nowrap'>{linkTitle?.value}</a>
 						</Link>
 					</div>
-					<div className='col-12 col-sm-6 col-lg-6 col-xl-8  '>
-						<button
-							onClick={() =>
-								whereToBuy.value &&
-								setChannelAdviserHandler(!channelAdviserHandler)
-							}
-							className='btn where-to-buy-bt btn-outline-light'>
-							{whereToBuy?.title}
-						</button>
-					</div>
+					{whereToBuy.value && (
+						<div className='col-12 col-sm-6 col-lg-6 col-xl-8  '>
+							<button
+								onClick={() =>
+									whereToBuy.value &&
+									setChannelAdviserHandler(!channelAdviserHandler)
+								}
+								className='btn where-to-buy-bt btn-outline-light'>
+								{whereToBuy?.title}
+							</button>
+						</div>
+					)}
 				</div>
 			</div>
 			{channelAdviserHandler && (
