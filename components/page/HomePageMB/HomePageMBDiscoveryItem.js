6d5d5442fd91a017dfac1ad456a32f1e886e002a
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-29 13:43:53 +0330

    model selection done

--- components/page/HomePageMB/HomePageMBDiscoveryItem.js
+++ components/page/HomePageMB/HomePageMBDiscoveryItem.js
@@ -7,19 +7,20 @@ const HomePageMBDiscoveryItem = ({ image, title, paragraph, link, width }) => {
 		<div
 			className={'home-page-mb-discovery-item py-10 px-6 px-md-13 py-md-15'}
 			style={{ width: width + '%' }}>
-			<img
-				height={'100px'}
-				src={image.src}
-				alt={image.alt}
-				className='mb-7'
-			/>
+			<img height={'100px'} src={image.src} alt={image.alt} className='mb-7' />
 			<h3 className='fs-base mb-7'>{title}</h3>
 			<p className='fs-base mb-9'>{paragraph}</p>
 			<a href={link.value} className='n-btn primary-text'>
 				{link.title}
-				<span>
-					<FontAwesomeIcon icon={faChevronRight} size={'sm'} className='ms-2' />
-				</span>
+				{link.value && (
+					<span>
+						<FontAwesomeIcon
+							icon={faChevronRight}
+							size={'sm'}
+							className='ms-2'
+						/>
+					</span>
+				)}
 			</a>
 		</div>
 	)
