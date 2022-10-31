1d13a84d4b968bc97ef0afd2c32cd23ec18832aa
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-31 16:54:53 +0330

    air products

---
+++ components/page/HomeAppliances/HomeApplianceImageWithBackground.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const HomeApplianceImageWithBackground = () => {
+	return <div></div>
+}
+
+export default HomeApplianceImageWithBackground
---
+++ utils/components/HomeAppliances.js
@@ -0,0 +1,22 @@
+module.exports.HomeAppliances = [
+	{
+		name: 'HomeApplianceFeatures',
+		path: 'components/page/HomeAppliances/HomeApplianceFeatures'
+	},
+	{
+		name: 'HomeApplianceQA',
+		path: 'components/page/HomeAppliances/HomeApplianceQA'
+	},
+	{
+		name: 'HomeApplianceTextBoxWithImage',
+		path: 'components/page/HomeAppliances/HomeApplianceTextBoxWithImage'
+	},
+	{
+		name: 'HomeApplianceTextBox',
+		path: 'components/page/HomeAppliances/HomeApplianceTextBox'
+	},
+	{
+		name: 'HomeApplianceImageWithBackground',
+		path: 'components/page/HomeAppliances/HomeApplianceImageWithBackground'
+	}
+]
