22e57fb1fb141b994505503e18599359c2755b49
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-10-31 17:19:06 +0330

    fix support

--- components/page/Support/SupportNeedMoreWithButton.js
+++ components/page/Support/SupportNeedMoreWithButton.js
@@ -1,37 +1,27 @@
 import React from 'react'
-
-// image
-import ImageBg from 'public/assets/images/support/support-bg.jpg'
+import Link from 'next/link'
 
 function SupportNeedMoreWithButton({ data }) {
-	let structure = {
-		image: {
-			src: '',
-			alt: ''
-		},
-		title: {
-			value: ''
-		},
-		link: {
-			title: '',
-			value: ''
-		}
-	}
+	let { structure } = data
 	return (
 		<section>
 			<div className='heading support-heading support-new-heading'>
-				<img src={structure.image.src} alt={structure.image.alt} width='100%' />
+				<img
+					src={structure?.image?.src}
+					alt={structure?.image?.alt}
+					width='100%'
+				/>
 				<div className='heading-text'>
 					<h4
 						className='text-white fs-2hx fw-normal mb-7'
 						style={{ color: '#fff' }}>
-						{structure.title.value}
+						{structure?.title?.value}
 					</h4>
-					<a
-						href={structure.link.value}
-						className='n-btn outline-white transparent'>
-						{structure.link.title}
-					</a>
+					<Link href={structure.link.value}>
+						<a className='n-btn outline-white transparent'>
+							{structure?.link?.title}
+						</a>
+					</Link>
 				</div>
 			</div>
 		</section>
