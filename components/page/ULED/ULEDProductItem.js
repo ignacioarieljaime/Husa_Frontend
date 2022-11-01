78829f839b43098a87c87725a0968d19a51c8d31
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-01 17:38:32 +0330

    fix uledItem count

--- components/page/ULED/ULEDProductItem.js
+++ components/page/ULED/ULEDProductItem.js
@@ -6,17 +6,24 @@ const ModalChanelAdviser = dynamic(() =>
 	import('../Product/ModalChanelAdviser')
 )
 
-function ULEDProductItem({ data }) {
+function ULEDProductItem({ data, itemLength }) {
 	let { title, description, image, whereToBuy, linkUrl, linkTitle, model } =
 		data
 	const [channelAdviserHandler, setChannelAdviserHandler] = useState(false)
 	return (
 		<>
-			<div className='col-12 col-lg-4 platform platform-uled platform-uled-item'>
+			<div
+				style={{ width: `${100 / itemLength}%` }}
+				className='platform platform-uled platform-uled-item'>
 				<h2>{title?.value}</h2>
 				<p className='text-black fs-2 fw-normal'>{description?.value}</p>
 				<div className='py-9 text-center'>
-					<img src={image?.src} alt={image?.alt} height='300px' />
+					<img
+						src={image?.src}
+						className={'w-100'}
+						alt={image?.alt}
+						style={{ maxHeight: '300px' }}
+					/>
 				</div>
 				<div className='row align-items-center'>
 					<div className='col-12 col-sm-3 discover-btn col-lg-6 col-xl-4  gap-3 mb-5 mb-sm-0'>
