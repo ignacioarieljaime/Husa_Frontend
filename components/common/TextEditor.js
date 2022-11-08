534c6e6d56a8aa28ec2aa4055e29e6d2368198fe
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-08 21:58:05 +0330

    fix text editor

--- components/common/TextEditor.js
+++ components/common/TextEditor.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 function TextEditor({ data }) {
 	let { structure } = data
+	console.log(data)
 	return (
 		<article
 			style={{ maxWidth: `${structure?.width?.value}px` }}
 			className={'article text-editor mx-auto pt-5 px-6 pb-6'}
-			dangerouslySetInnerHTML={{ __html: structure?.text?.value }}></article>
+			dangerouslySetInnerHTML={{
+				__html: structure?.text?.value
+					? structure?.text?.value
+					: structure?.text
+			}}></article>
 	)
 }
 
