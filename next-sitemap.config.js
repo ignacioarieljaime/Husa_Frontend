4002a95c2189e18093f1795abe9df0e9374fe7fe
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-15 23:41:13 +0330

    completed site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: process.env.SITE_URL || 'https://public.dev.hisenseportal.com/',
+	generateRobotsTxt: true // (optional)
+	// ...other options
+}
