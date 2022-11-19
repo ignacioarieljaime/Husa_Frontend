8642797f6d1c74675812eb1989c9aeddbeb75707
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-19 13:51:18 +0330

    change blog request type

--- Controller/ComponentController.js
+++ Controller/ComponentController.js
@@ -118,13 +118,7 @@ const GenerateComponentStructure = (_page, _content, _condition) => {
 			: `  export async function getServerSideProps(context) {
 				console.log('send cxm request')
 				let data = await axios
-					.get(
-						'https://imcxm.dev-api.hisenseportal.com/api/post/${_page.id}' ,{
-							headers: {
-								Authorization:
-									'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiMTM3NTJkNmJmNzE5OTRjNDRmYmU2OWY5OWIwZGE0ZGY2NDhlYjkxYTRiOWZlMDZkYmE5YmJlNzk3MGE4NGM0MTk0ZDNjYTEzYTgzNTVhYmQiLCJpYXQiOjE2NjY2NDAyNjEuODM4ODY1LCJuYmYiOjE2NjY2NDAyNjEuODM4ODY3LCJleHAiOjE2OTgxNzYyNjEuODI4ODk0LCJzdWIiOiI1Iiwic2NvcGVzIjpbXX0.QlOepcxHTAs2uz_RC9DGixoRaoAcXdEFJ9PKHWob-KONfIWVoTlnkRpElmJfRzHbXuXIe40c6xvSvxEaraYCJjh5QwHeSrU-IrkfI_477Pb92SxdVeElGMiSKS33wEcHU0-DovkHn60EyOYQ3vpVqvUBsCPMrLbUYW-JB2nGyj19DDh0UR-uo8b2gWXq3KA34RnvTU0R6MQi7K38GuksKiTcj0thyDkQmA8PelxWMco4GOCq_f7zDniMuVmBxTWmlFWOKD-8ero-Q3h-NipTZGCGj6ER43euN7wMassyTrqu4GywNl3Cq2sdder0JPX-MBX3G3igL7GZzWEjwgxi54nmcmWiOgD-v7h0G0Jd17y2KRrJzZAt3rgpNfJB5PIqDJHWyEFrlVb6lT4jHaxmZEpSVdZV9pMTF1DX8isywiC2j8YmU4nKBgU07YMeU1xgESfmcpXrcZx_ISGRu4_haDy7vcPtznT45cto7Nughwj7S3Ef2Q_qCWB0ZekfIejNiUQy6Jy91v0HbAq7_mHsfhrjIo9uGRg_zr0aNpU1iK2ahKT-WPGJ8hDyriVsqRNxcYoUj7aOibus7xPm0zRHslf_uuY63K40jmcyrb8RvzEIJQDl_aRAXkyxMlcsWHKGaHyP3yw0E9SVm19yvshjEkH3stlK8NhBCAJWMkZCZWE'
-							}
-						})
+					.get('${process.env.CXM_API_ROUTE}/getPostInfo/${_page.id}')
 					.then(response => {
 						console.log('get cxm blog data')
 						return response.data.widgets
