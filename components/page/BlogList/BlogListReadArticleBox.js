25cb0035a0be9ae0784a088fcc39129b81426977
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-20 14:43:05 +0330

    fix blog error

--- components/page/BlogList/BlogListReadArticleBox.js
+++ components/page/BlogList/BlogListReadArticleBox.js
@@ -1,55 +1,53 @@
 import React from 'react'
-
-// image
-import HouseImage from 'public/assets/images/blog/house.png'
 import Link from 'next/link'
 
-function BlogListReadArticleBox({ data: { structure } }) {
-	return (
-		<section>
-			<div className='container'>
-				<div className='row mb-20 pb-0 pb-md-20'>
-					<div className='col-12'>
-						<figure className='blog-article-box'>
-							<div className='header-secondary image-box'>
-								<Link href={structure?.link?.value}>
-									<a>
-										<img
-											src={structure?.image?.src}
-											alt={structure?.image?.alt}
-											width='100%'
-											height='100%'
-										/>
-									</a>
-								</Link>
-							</div>
-							<figcaption>
-								<div className='row justify-content-between align-items-center mt-5'>
-									<div className='col-12 col-md-9 order-2 order-md-1 mb-8'>
-										<h3 className='fs-3 mb-0'>{structure?.title?.value}</h3>
-									</div>
-									<div className='col-12 col-md-3 order-1 order-md-2 text-start text-md-end mb-3 mb-md-8'>
-										<Link href={structure?.tagLink?.value}>
-											<a className='text-primary-dark text-decoration-none'>
-												{structure?.tagLink?.title}
-											</a>
-										</Link>
-									</div>
-									<div className='col-12 order-3 text-start'>
-										<Link href={structure?.link?.value}>
-											<a className='btn btn-outline-dark green-hover px-6 py-3 rounded-5 text-uppercase'>
-												{structure?.link?.title}
-											</a>
-										</Link>
-									</div>
-								</div>
-							</figcaption>
-						</figure>
-					</div>
-				</div>
-			</div>
-		</section>
-	)
+function BlogListReadArticleBox({data: {structure}}) {
+    return (
+        <section>
+            <div className='container'>
+                <div className='row mb-20 pb-0 pb-md-20'>
+                    <div className='col-12'>
+                        <figure className='blog-article-box'>
+                            <div className='header-secondary image-box'>
+                                <Link href={structure?.link?.value ? structure?.link?.value : "/"}>
+                                    <a>
+                                        <img
+                                            src={structure?.image?.src}
+                                            alt={structure?.image?.alt}
+                                            width='100%'
+                                            height='100%'
+                                        />
+                                    </a>
+                                </Link>
+                            </div>
+                            <figcaption>
+                                <div className='row justify-content-between align-items-center mt-5'>
+                                    <div className='col-12 col-md-9 order-2 order-md-1 mb-8'>
+                                        <h3 className='fs-3 mb-0'>{structure?.title?.value}</h3>
+                                    </div>
+                                    <div
+                                        className='col-12 col-md-3 order-1 order-md-2 text-start text-md-end mb-3 mb-md-8'>
+                                        <Link href={structure?.tagLink?.value ? structure?.tagLink?.value : "/"}>
+                                            <a className='text-primary-dark text-decoration-none'>
+                                                {structure?.tagLink?.title}
+                                            </a>
+                                        </Link>
+                                    </div>
+                                    <div className='col-12 order-3 text-start'>
+                                        <Link href={structure?.link?.value ? structure?.link?.value : "/"}>
+                                            <a className='btn btn-outline-dark green-hover px-6 py-3 rounded-5 text-uppercase'>
+                                                {structure?.link?.title}
+                                            </a>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default BlogListReadArticleBox
--- components/page/BlogList/BlogListSoundBardItem.js
+++ components/page/BlogList/BlogListSoundBardItem.js
@@ -2,49 +2,49 @@ import React from 'react'
 
 import Link from 'next/link'
 
-function BlogListSoundBardItem({ data }) {
-	return (
-		<div className='row align-items-center py-8 border-bottom mx-0 blog-article-box'>
-			<figure className='col-12 col-md-9 mb-0 px-0'>
-				<div className='row justify-content-between align-items-center'>
-					<div className='col-12 col-md-4 col-lg-3 mb-5 mb-md-0'>
-						<div className='image-box'>
-							<Link href={data?.link?.value}>
-								<a>
-									<img
-										src={data?.image?.src}
-										alt={data?.image?.alt}
-										width='100%'
-									/>
-								</a>
-							</Link>
-						</div>
-					</div>
-					<figcaption className='col-12 col-md-8'>
-						<div className='text-start mb-3'>
-							<Link href={data?.tagLink?.value}>
-								<a className='text-primary-dark text-decoration-none'>
-									{data?.tagLink?.title}
-								</a>
-							</Link>
-						</div>
-						<div className='mb-8 mb-md-0'>
-							<h3 className='fs-3 mb-0'>{data?.title?.value}</h3>
-						</div>
-					</figcaption>
-				</div>
-			</figure>
-			<div className='col-12 col-md-3 px-0'>
-				<div className='text-start text-md-end'>
-					<Link href={data?.link?.value}>
-						<a className='btn btn-outline-dark green-hover px-6 py-3 rounded-5 text-uppercase'>
-							{data?.link?.title}
-						</a>
-					</Link>
-				</div>
-			</div>
-		</div>
-	)
+function BlogListSoundBardItem({data}) {
+    return (
+        <div className='row align-items-center py-8 border-bottom mx-0 blog-article-box'>
+            <figure className='col-12 col-md-9 mb-0 px-0'>
+                <div className='row justify-content-between align-items-center'>
+                    <div className='col-12 col-md-4 col-lg-3 mb-5 mb-md-0'>
+                        <div className='image-box'>
+                            <Link href={data?.link?.value ? data?.link?.value : "/"}>
+                                <a>
+                                    <img
+                                        src={data?.image?.src}
+                                        alt={data?.image?.alt}
+                                        width='100%'
+                                    />
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                    <figcaption className='col-12 col-md-8'>
+                        <div className='text-start mb-3'>
+                            <Link href={data?.tagLink?.value ? data?.tagLink?.value : "/"}>
+                                <a className='text-primary-dark text-decoration-none'>
+                                    {data?.tagLink?.title}
+                                </a>
+                            </Link>
+                        </div>
+                        <div className='mb-8 mb-md-0'>
+                            <h3 className='fs-3 mb-0'>{data?.title?.value}</h3>
+                        </div>
+                    </figcaption>
+                </div>
+            </figure>
+            <div className='col-12 col-md-3 px-0'>
+                <div className='text-start text-md-end'>
+                    <Link href={data?.link?.value ? data?.link?.value : "/"}>
+                        <a className='btn btn-outline-dark green-hover px-6 py-3 rounded-5 text-uppercase'>
+                            {data?.link?.title}
+                        </a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default BlogListSoundBardItem
