b8d9af3baee6dd669d806696b75cb1537d790aed
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-20 18:47:37 +0330

    fix BlogDescriptionWithShare component

--- components/page/Blog/BlogDescriptionWithShare.js
+++ components/page/Blog/BlogDescriptionWithShare.js
@@ -2,21 +2,21 @@ import React from 'react'
 
 function BlogDescriptionWithShare({data: {structure}}) {
     return (<section>
-        <div className='tiny-banner container py-md-20 my-20'>
-            <article className='blog-article'>
-                <div
-                    dangerouslySetInnerHTML={{__html: structure?.text?.value}}></div>
-                <span>{structure?.sharingTitle?.value}</span>
-                <div className='row mt-7'>
-                    {structure?.list?.value.map((item, index) => (<a
-                        href={item?.link?.value}
-                        key={index}
-                        className={`text-primary-dark socicon socicon-${item?.socialMedia?.value}`}>
-                    </a>))}
-                </div>
-            </article>
-        </div>
-    </section>)
+            <div className='tiny-banner container py-md-20 my-20'>
+                <article className='blog-article'>
+                    <div
+                        dangerouslySetInnerHTML={{__html: structure?.text?.value}}></div>
+                    <span>{structure?.sharingTitle?.value}</span>
+                    <div className='row mt-7'>
+                        {structure?.list?.value.map((item, index) => (<a
+                            href={item?.link?.value}
+                            key={index}
+                            className={`text-primary-dark socicon socicon-${item?.socialMedia?.value}`}>
+                        </a>))}
+                    </div>
+                </article>
+            </div>
+        </section>)
 }
 
 export default BlogDescriptionWithShare
