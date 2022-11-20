6038e11db5bc36c7d9615f1f526ec9e86d8ee99c
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-20 18:49:16 +0330

    fix sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const {withSentryConfig} = require('@sentry/nextjs')
 
 const moduleExports = {
-    // Your existing module.exports
     reactStrictMode: true,
     swcMinify: true,
     env: {
@@ -10,41 +9,12 @@ const moduleExports = {
             'https://imcxm.stage-api.hisenseportal.com/api/husa'
     },
     sentry: {
-        // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-        // for client-side builds. (This will be the default starting in
-        // `@sentry/nextjs` version 8.0.0.) See
-        // https://webpack.js.org/configuration/devtool/ and
-        // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-        // for more information.
         hideSourceMaps: true
     }
 }
 
 const sentryWebpackPluginOptions = {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
-
-    silent: true // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
+    silent: true
 }
-const nextConfig = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
 
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-// module.exports = withSentryConfig(
-// 	moduleExports,
-// 	sentryWebpackPluginOptions,
-// 	nextConfig,
-// 	env: {
-// 		NEXT_PUBLIC_CXM_API_ROUTE:
-// 			process.env.CXM_API_ROUTE ||
-// 			'https://imcxm.stage-api.hisenseportal.com/api/husa'
-// 	}
-// )
-
-module.exports = nextConfig
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
