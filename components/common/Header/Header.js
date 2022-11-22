7a21a05035eb5f98916800908bbcadd27a0525ab
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-22 12:16:47 +0330

    fix header notification listImageBox productInfoAndSlider productInfo RegisterForm

--- components/common/Header/Header.js
+++ components/common/Header/Header.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 // component
 import HamburgerMenu from './HamburgerMenu'
@@ -13,112 +13,118 @@ import NavBarDropDown from './NavBarDropDown'
 import Link from 'next/link'
 import HeaderSearchBox from './HeaderSearchBox'
 
-function Header({isBlog = false, data: {structure}}) {
-    // const { headerData } = useSelector(state => state.layoutData)
-    const [headerData, setHeaderData] = useState()
-    const [asideMenu, setAsideMenu] = useState(false)
-    const [topNavCondition, setTopNavCondition] = useState(false)
-    const [searchInputCondition, setSearchInputCondition] = useState(false)
-    useEffect(() => {
-        sessionStorage.getItem('headerData')
-            ? setHeaderData(JSON.parse(sessionStorage.getItem('headerData')))
-            : getMenu()
-    }, [])
-    useEffect(() => {
-        if (!searchInputCondition) {
-            setTimeout(() => {
-                setTopNavCondition(true)
-            }, 800)
-        } else {
-            setTopNavCondition(false)
-        }
-    }, [searchInputCondition])
+function Header({ data: { structure } }) {
+	// const { headerData } = useSelector(state => state.layoutData)
+	const [headerData, setHeaderData] = useState()
+	const [asideMenu, setAsideMenu] = useState(false)
+	const [topNavCondition, setTopNavCondition] = useState(false)
+	const [searchInputCondition, setSearchInputCondition] = useState(false)
+	console.log(structure?.notification)
+	useEffect(() => {
+		sessionStorage.getItem('headerData')
+			? setHeaderData(JSON.parse(sessionStorage.getItem('headerData')))
+			: getMenu()
+	}, [])
+	useEffect(() => {
+		if (!searchInputCondition) {
+			setTimeout(() => {
+				setTopNavCondition(true)
+			}, 800)
+		} else {
+			setTopNavCondition(false)
+		}
+	}, [searchInputCondition])
 
-    const getMenu = async () => {
-        try {
-            let response = await axios.get(
-                `${process.env.NEXT_PUBLIC_CXM_API_ROUTE}/getMenus`
-            )
-            setHeaderData(response.data.data.find(item => item.title === 'header'))
-            sessionStorage.setItem(
-                'headerData',
-                JSON.stringify(response.data.data.find(item => item.title === 'header'))
-            )
-        } catch (error) {
-            console.log(error)
-        }
-    }
+	const getMenu = async () => {
+		try {
+			let response = await axios.get(
+				`${process.env.NEXT_PUBLIC_CXM_API_ROUTE}/getMenus`
+			)
+			setHeaderData(response.data.data.find(item => item.title === 'header'))
+			sessionStorage.setItem(
+				'headerData',
+				JSON.stringify(response.data.data.find(item => item.title === 'header'))
+			)
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
-    return (
-        <header>
-            <nav
-                style={{transitionDelay: '2s'}}
-                className={`navbar navbar-expand justify-content-center  
+	return (
+		<header>
+			<nav
+				style={{ transitionDelay: '2s' }}
+				className={`navbar navbar-expand justify-content-center  
 					theme-${structure.theme.value}
 				 top-nav py-3 px-sm-4 fixed-top flex-wrap ${
-                    !topNavCondition && 'search-mode'
-                } ${asideMenu || searchInputCondition ? 'hidden' : ''}`}>
-                <div className='container-fluid'>
-                    <div className='row justify-content-between align-items-center w-100 m-auto'>
-                        <Link href='/'>
-                            <a className='navbar-brand m-0 col-xl-4 text-start'>
-                                <Logo/>
-                            </a>
-                        </Link>
-                        <h1>Hisense</h1>
-                        <ul className='navbar-nav col-xl-4 d-none d-lg-flex justify-content-center p-0'>
-                            {headerData?.widgets?.centerOption.map((item, index) => (
-                                <NavBarDropDown key={`right-${index}`} data={item}/>
-                            ))}
-                        </ul>
-                        <ul className='navbar-nav align-items-center justify-content-end p-0 col-4'>
-                            {headerData?.widgets?.rightOption.map((item, index) => (
-                                <NavBarDropDown key={`right-${index}`} data={item}/>
-                            ))}
+					!topNavCondition && 'search-mode'
+				} ${asideMenu || searchInputCondition ? 'hidden' : ''}`}>
+				<div className='container-fluid'>
+					<div className='row justify-content-between align-items-center w-100 m-auto'>
+						<Link href='/'>
+							<a className='navbar-brand m-0 col-xl-4 text-start'>
+								<Logo />
+							</a>
+						</Link>
+						<h1>Hisense</h1>
+						<ul className='navbar-nav col-xl-4 d-none d-lg-flex justify-content-center p-0'>
+							{headerData?.widgets?.centerOption.map((item, index) => (
+								<NavBarDropDown key={`right-${index}`} data={item} />
+							))}
+						</ul>
+						<ul className='navbar-nav align-items-center justify-content-end p-0 col-4'>
+							{headerData?.widgets?.rightOption.map((item, index) => (
+								<NavBarDropDown key={`right-${index}`} data={item} />
+							))}
 
-                            <li className='nav-item-button' data-button='search'>
-                                <button
-                                    className={`btn search-icon p-2  ${
-                                        searchInputCondition && 'open'
-                                    }`}
-                                    onClick={() => setSearchInputCondition(!searchInputCondition)}
-                                    aria-label='search'>
-                                    <MagnifierIcon/>
-                                </button>
-                            </li>
-                            <li className='nav-item-button' data-button='menu'>
-                                <button
-                                    className={`btn hamburger-icon p-2 ${
-                                        asideMenu || searchInputCondition ? 'close' : ''
-                                    }`}
-                                    onClick={() =>
-                                        searchInputCondition
-                                            ? setSearchInputCondition(false)
-                                            : setAsideMenu(!asideMenu)
-                                    }
-                                    aria-label='hamburger menu toggler'>
-                                    <HamburgerMenuIcon/>
-                                </button>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <HeaderSearchBox
-                    theme={structure.theme.value}
-                    searchInputCondition={searchInputCondition}
-                />
+							<li className='nav-item-button' data-button='search'>
+								<button
+									className={`btn search-icon p-2  ${
+										searchInputCondition && 'open'
+									}`}
+									onClick={() => setSearchInputCondition(!searchInputCondition)}
+									aria-label='search'>
+									<MagnifierIcon />
+								</button>
+							</li>
+							<li className='nav-item-button' data-button='menu'>
+								<button
+									className={`btn hamburger-icon p-2 ${
+										asideMenu || searchInputCondition ? 'close' : ''
+									}`}
+									onClick={() =>
+										searchInputCondition
+											? setSearchInputCondition(false)
+											: setAsideMenu(!asideMenu)
+									}
+									aria-label='hamburger menu toggler'>
+									<HamburgerMenuIcon />
+								</button>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<HeaderSearchBox
+					theme={structure.theme.value}
+					searchInputCondition={searchInputCondition}
+				/>
 
-                {/* <SubMenuHeader /> */}
-            </nav>
+				{/* <SubMenuHeader /> */}
+			</nav>
+			{
+				structure?.notification?.value?.link?.value && <div className='container-fluid home-top-advertisement'>
+					<Link href={structure?.notification?.value?.link?.value}>
+						<a>
+							{structure?.notification?.value?.title?.value}
+						</a>
+					</Link>
+				</div>
+			}
 
-            {/* <div className='container-fluid home-top-advertisement'>
-				<a href='https://www.hisense-usa.com/product-safety-recall'>
-					Recall Information: French Door Refrigerator
-				</a>
-			</div> */}
-            {headerData && <HamburgerMenu data={headerData} asideMenu={asideMenu}/>}
-        </header>
-    )
+
+			{headerData && <HamburgerMenu data={headerData} asideMenu={asideMenu} />}
+		</header>
+	)
 }
 
 export default Header
--- components/page/Product/ProductInfoAndSliderBox.js
+++ components/page/Product/ProductInfoAndSliderBox.js
@@ -15,10 +15,12 @@ function ProductInfoAndSliderBox({ pim, data }) {
 				<div className='row'>
 					<ProductInfoSlider firstImage={pim?.image} pim={pim?.assets} />
 					<div className='col-12 col-md-6 product-info my-auto'>
-						<h2 className='text-primary fs-2x mb-5'>
+						<h2
+							className={`${pim?.custom_fields.find(item => item.title === 'h2 Title')?.value ? 'text-primary fs-2x mb-5' : 'text-black fs-2hx mb-1'} `}>
 							{
 								pim?.custom_fields.find(item => item.title === 'h2 Title')
-									?.value
+									?.value ? pim?.custom_fields.find(item => item.title === 'h2 Title')?.value
+									?.value : pim?.custom_fields?.find(item => item.title === 'Product Type')?.value
 							}
 						</h2>
 						<h1 className='fs-2hx mb-8'>
