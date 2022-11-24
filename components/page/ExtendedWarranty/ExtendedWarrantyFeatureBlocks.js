c988425fc912151ada2456ae80f368d5d9d44634
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-24 18:28:34 +0330

    fix pdp QA

--- components/page/ExtendedWarranty/ExtendedWarrantyFeatureBlocks.js
+++ components/page/ExtendedWarranty/ExtendedWarrantyFeatureBlocks.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ExtendedWarrantyFeatureBlocksItem from './ExtendedWarrantyFeatureBlocksItem'
 
 const ExtendedWarrantyFeatureBlocks = ({ data: { structure } }) => {
+	console.log(structure)
 	return (
 		<section>
 			<div
 				className='home-page-mb-discovery py-20'
-				style={{ backgroundColor: structure?.backgroundColor?.value }}>
-				{structure?.backgroundColor?.value ? (
+				style={{ backgroundColor: structure?.backgroundColor?.value ? structure?.backgroundColor?.value : '#F0F2F2' }}>
+				{structure?.title?.value ? (
 					<h2 className='fs-2hx mb-17'>{structure?.title?.value}</h2>
 				) : null}
 				<div className='row justify-content-evenly align-items-center mx-0'>
