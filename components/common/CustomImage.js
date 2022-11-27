52da2b31dd34516ee6be807126effb09acc690c2
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-27 20:27:57 +0330

    completed support components Images

--- components/common/CustomImage.js
+++ components/common/CustomImage.js
@@ -2,16 +2,25 @@ import React from 'react'
 import Image from 'next/image'
 
 function CustomImage({
-											 alt, src, className, style, layout = 'fill', wrapperWidth, wrapperHeight, onLoad = () => {
-	}
+											 alt,
+											 src,
+											 className="",
+											 style,
+											 title = '',
+											 layout = 'fill',
+											 wrapperWidth,
+											 wrapperHeight, wrapperClass,
+											 onLoad = () => {
+											 }
 										 }) {
 	return (
-		<div className={className}  onLoad={() => onLoad()}
+		<div className={`${className} ${wrapperClass}`} onLoad={() => onLoad()}
 				 style={{ ...style, height: wrapperHeight, width: wrapperWidth, position: 'relative' }}>
 			<Image
 				layout={layout}
 				src={src}
 				alt={alt}
+				title={title}
 				style={{ position: 'relative' }}
 				className={className}
 			/>
