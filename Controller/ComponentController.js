ab652d2a6c2ea9163aba7d56cd41937606d1aa16
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-28 09:07:30 +0330

    server bug

--- Controller/ComponentController.js
+++ Controller/ComponentController.js
@@ -6,7 +6,8 @@ const FindComponent = _componentData => {
 		? _componentData.map(item => {
 			let pageComponents = ComponentList.find(pageComponent => {
 				if (pageComponent.name === item.name) {
-					pageComponent.structure = item?.structure
+					pageComponent.id = item?.id;
+					pageComponent.structure = item?.structure;
 					return pageComponent
 				}
 			})
@@ -28,118 +29,100 @@ const GenerateComponentStructure = (_page, _content, _condition) => {
 	${uniqueImport.map(item => item && `const ${item.name} = dynamic(() => import('${item.path}'))`).join(';')}
 
 	function Index${_page.id}({pim,data}) {
-		console.log('build data', ${JSON.stringify(_page)})
-	console.log('server side data' , data)
-	const router = useRouter()
+		const router = useRouter()
 
 		${
 		_page?.redirect
 			? `useEffect(() => {
 			router.push('${_page.redirect}')
-	}, [])`
-			:
-			''
+		}, [])`
+			: ''
 	}
-return (
-	<Layout title={'${_page.title}'} meta={${
+    return (
+			<Layout title={'${_page.title}'} meta={${
 		_condition === 'pages' ? _page.meta : JSON.stringify(_page.meta)
 	}}>
-		<section>
-			${_content
+	      		<section>
+		  		 ${_content
 		.map(
 			(item, index) =>
 				item &&
-				`{data && data.length > 0 && data[${index}]?.structure ? <${
+				`{data && data.length > 0 && data.find(componentItem => componentItem.id === ${item.id} && componentItem.name === '${item.name}') ? <${
 					item.name
 				} ${
 					_page.model_type ? `pim={pim}` : ''
-				} data={data[${index}]}/>  : null }`
+				} data={data.find(componentItem => componentItem.id === ${item.id} && componentItem.name === '${item.name}')}/>  : null }`
 		)
 		.join(' ')}
-		</section>
-	</Layout>
-)
-}
+				</section>
+			</Layout>
+	    )
+	  }
 
-${
+	${
 		_condition === 'pages'
 			? _page.model_type
 				? `  export async function getServerSideProps(context) {
-		console.log('send cxm request')
-		let data = await axios
-			.get(
-				'${process.env.CXM_API_ROUTE}
-/getPageInfo/${_page.id}
-'
-)
-.
-then(response => {
-	console.log('get cxm data')
-	return response.data.widgets
-})
-	.catch(error => {
-		console.error('Error:', error)
-		return null
-	})
-console.log('send pim request')
-let pim = await axios
-	.get(
-		'https://impim.dev-api.hisenseportal.com/api/cms/getProduct/${_page.model_id}'
-	)
-	.then(response => {
-		console.log('get pim data')
-		return response.data.data
-	})
-	.catch(error => {
-		console.error('Error:', error)
-		return null
-	})
+			console.log('send cxm request')
+			let data = await axios
+				.get(
+					'${process.env.CXM_API_ROUTE}/getPageInfo/${_page.id}'
+				)
+				.then(response => {
+					console.log('get cxm data')
+					return response.data.widgets
+				})
+				.catch(error => {
+					console.error('Error:', error)
+					return null
+				})			
+				console.log('send pim request')
+				 let pim = await axios
+						.get(
+							'https://impim.dev-api.hisenseportal.com/api/cms/getProduct/${_page.model_id}'
+						)
+						.then(response => {
+							console.log('get pim data')
+							return response.data.data
+						})
+						.catch(error => {
+							console.error('Error:', error)
+							return null
+						})
+	
+		
+			return { props: { data , pim }} }`
+				: `  export async function getServerSideProps(context) {
+			console.log('send ssr request')
+			let data = await axios
+				.get(
+					'${process.env.CXM_API_ROUTE}/getPageInfo/${_page.id}'
+				)
+				.then(response => {
+					console.log('get ssr data')
+					return response.data.widgets
+				})
+				.catch(error => {
+					console.error('Error:', error)
+					return null
+				})			
 
-
-return { props: { data, pim } }
-}
-`
-				: `
-
-export async function getServerSideProps(context) {
-	console.log('send ssr request')
-	let data = await axios
-		.get(
-			'${process.env.CXM_API_ROUTE}/getPageInfo/${_page.id}'
-		)
-		.then(response => {
-			console.log('get ssr data')
-			return response.data.widgets
-		})
-		.catch(error => {
-			console.error('Error:', error)
-			return null
-		})
-
-	return { props: { data } }
-}
-
-`
-			: `
-
-export async function getServerSideProps(context) {
-	console.log('send cxm request')
-	let data = await axios
-		.get('${process.env.CXM_API_ROUTE}/getPostInfo/${_page.id}')
-		.then(response => {
-			console.log('get cxm blog data')
-			return response.data.widgets
-		})
-		.catch(error => {
-			console.error('Error:', error)
-			return null
-		})
-
-
-	return { props: { data } }
-}
-
-`
+			return { props: { data }} }`
+			: `  export async function getServerSideProps(context) {
+				console.log('send cxm request')
+				let data = await axios
+					.get('${process.env.CXM_API_ROUTE}/getPostInfo/${_page.id}')
+					.then(response => {
+						console.log('get cxm blog data')
+						return response.data.widgets
+					})
+					.catch(error => {
+						console.error('Error:', error)
+						return null
+					})			
+					
+			
+				return { props: { data }} }`
 	}
 	
 
