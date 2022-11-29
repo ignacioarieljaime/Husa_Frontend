21cae8d450ddf50db1c620206975e298b7104422
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-29 12:57:23 +0330

    refactor sentry

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,16 +1,17 @@
-import * as Sentry from '@sentry/nextjs'
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-	dsn:
-		SENTRY_DSN ||
-		'https://70739967e49c4d6b9b863ca6d3e3ad8a@o4504095127502848.ingest.sentry.io/4504128249856000',
-	// We recommend adjusting this value in production, or using tracesSampler
-	// for finer control
-	tracesSampleRate: 1.0
-	// ...
-	// Note: if you want to override the automatic release value, do not set a
-	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
-	// that it will also get attached to your source maps
-})
+  dsn:'https://70739967e49c4d6b9b863ca6d3e3ad8a@o4504095127502848.ingest.sentry.io/4504128249856000',
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
