659534e37e58fb2110d9b5d3fad7cd3dad66d171
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-11-29 12:28:23 +0330

    updateSentry

--- config/RemovePages.js
+++ config/RemovePages.js
@@ -5,7 +5,7 @@ fs.readdir(path.resolve(__dirname, '../pages'), (err, files) => {
 	if (err) throw err
 	if (files.includes('index.js')) {
 		files.forEach(file => {
-			if (file !== '404.js' && file !== '_app.js') {
+			if (file !== '404.js' && file !== '_app.js' && file !== '_error.js') {
 				fs.rm(
 					path.resolve(__dirname, '../pages', file),
 					{ recursive: true, force: true },
--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,8 +3,14 @@ import * as Sentry from '@sentry/nextjs'
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-	dsn: 'https://09bf76ce59be40c5a315a038227449a5@o4504095127502848.ingest.sentry.io/4504128249856000',
-	tracesSampleRate: 1.0,
-	environment: process.env.NODE_ENV,
-	debug: false,
+	dsn:
+		SENTRY_DSN ||
+		'https://70739967e49c4d6b9b863ca6d3e3ad8a@o4504095127502848.ingest.sentry.io/4504128249856000',
+	// We recommend adjusting this value in production, or using tracesSampler
+	// for finer control
+	tracesSampleRate: 1.0
+	// ...
+	// Note: if you want to override the automatic release value, do not set a
+	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
+	// that it will also get attached to your source maps
 })
