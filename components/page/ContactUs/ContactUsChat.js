8d39cfb7bc6b70173704937f303e53fb02cdf896
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-12-05 11:20:57 +0330

    wip

---
+++ components/page/ContactUs/ContactUsChat.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ContactUsChat() {
+	return (
+		<section className='container contact_us_chat'>
+			<div>
+				<h5>Lets chat</h5>
+				<h6>سسسس</h6>
+			</div>
+			<button>OPEN CHAT</button>
+		</section>
+	)
+}
+
+export default ContactUsChat
