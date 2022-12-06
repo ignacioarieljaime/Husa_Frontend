3ca97c065083b87510489cf4533878c2d773045f
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-12-06 09:41:38 +0330

    fix warranty

--- components/page/ExtendedWarranty/ExtendedWarrantyFAQAccordion.js
+++ components/page/ExtendedWarranty/ExtendedWarrantyFAQAccordion.js
@@ -38,26 +38,26 @@ const ExtendedWarrantyFAQAccordion = ({ title, questions }) => {
 				</span>
 			</button>
 			<div className='faq-accordion-content' ref={accordionContent}>
-				{questions.map((item, index) => (
-					<div
-						key={index}
-						ref={accordionContentHeight}
-						className='faq-accordion-question-item'>
-						<h4 className='faq-accordion-question'>
-							{item?.question?.value}
-							<button onClick={() => setCollapsed(collapsed => !collapsed)}>
-								<FontAwesomeIcon
-									icon={faCircleXmark}
-									size={'xl'}
-									className='ms-2'
-								/>
-							</button>
-						</h4>
-						<div
-							className='faq-accordion-answer'
-							dangerouslySetInnerHTML={{ __html: item?.answer?.value }}></div>
-					</div>
-				))}
+				<div ref={accordionContentHeight}>
+					{' '}
+					{questions.map((item, index) => (
+						<div key={index} className='faq-accordion-question-item'>
+							<h4 className='faq-accordion-question'>
+								{item?.question?.value}
+								<button onClick={() => setCollapsed(collapsed => !collapsed)}>
+									<FontAwesomeIcon
+										icon={faCircleXmark}
+										size={'xl'}
+										className='ms-2'
+									/>
+								</button>
+							</h4>
+							<div
+								className='faq-accordion-answer'
+								dangerouslySetInnerHTML={{ __html: item?.answer?.value }}></div>
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	)
