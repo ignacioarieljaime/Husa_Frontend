241690f7fd0506578ab67d55de6cd100f423e084
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-12-21 00:12:03 +0330

    remove edit in ProductFeatureWithImage

--- components/page/Product/ProductFeatureWithImage.js
+++ components/page/Product/ProductFeatureWithImage.js
@@ -7,31 +7,16 @@ function ProductFeatureWithImage({ data }) {
 		<section>
 			<div
 				style={{ background: structure?.backgroundColor?.value }}
-				className={`row description-blocks  ${
+				className={`row description-blocks theme-${structure?.theme?.value} ${
 					structure?.theme?.value === 'dark' ? 'text-white' : ''
 				} align-items-center ${
 					structure?.direction?.value === 'rtl' ? 'flex-row-reverse' : ''
 				}`}>
 				<article className='introduction col-12 col-md-6 order-2'>
-					<h4
-						style={{
-							color: structure?.theme?.value === 'dark' ? '#fff' : '#000'
-						}}>
-						{structure?.littleTitle?.value}
-					</h4>
+					<h4>{structure?.littleTitle?.value}</h4>
 
-					<h2
-						style={{
-							color: structure?.theme?.value === 'dark' ? '#fff' : '#000'
-						}}>
-						{structure?.title?.value}
-					</h2>
-					<p
-						style={{
-							color: structure?.theme?.value === 'dark' ? '#fff' : '#000'
-						}}>
-						{structure?.description?.value}
-					</p>
+					<h2>{structure?.title?.value}</h2>
+					<p>{structure?.description?.value}</p>
 				</article>
 				<CustomImage
 					src={structure?.image?.src}
