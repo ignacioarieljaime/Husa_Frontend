9589e1dafe640ed727e19b22e8f63d8646bdd327
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-12-20 18:11:47 +0330

    fix ProductPackageCategoryBox

--- components/page/Product/ProductPackageCategoryBox.js
+++ components/page/Product/ProductPackageCategoryBox.js
@@ -32,7 +32,13 @@ function ProductPackageCategoryBox({ data, pim }) {
 				ref={menu}>
 				<nav className=''>
 					<ul className='row justify-content-evenly justify-content-md-start align-items-center p-0 m-0'>
-						{structure?.tags?.value.map((item, index) => (
+						<li className='me-md-auto'>
+							<span className='underline-on-hover text-uppercase'>
+								{pim?.model}
+							</span>
+						</li>
+
+						{/* {structure?.tags?.value.map((item, index) => (
 							<li key={`category-item-${index}`}>
 								<Link href={item.target.value ? item.target.value : '/'}>
 									<a>
@@ -60,7 +66,7 @@ function ProductPackageCategoryBox({ data, pim }) {
 									</span>
 								</a>
 							</Link>
-						</li>
+						</li> */}
 						<li className='ms-md-auto '>
 							<button
 								style={{ padding: fix ? '20px 20px' : '14px 20px' }}
