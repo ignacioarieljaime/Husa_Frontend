6309f5de72c0bc1400a884e2907368da63cf95ab
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-12-21 09:52:04 +0330

    fix spotlightitem

--- components/page/NewHomePage/BlockSpotlightItem.js
+++ components/page/NewHomePage/BlockSpotlightItem.js
@@ -31,18 +31,14 @@ const BlockSpotlightItem = ({ data }) => {
 					/>
 				</a>
 			</Link>
-			<h5 className='description'>{seriesTitle && product?.model}</h5>
+			<h5 className='description'>{product?.model}</h5>
 			<h3 className='title'>
 				{seriesTitle
 					? seriesTitle?.custom_fields.find(item => item.name === 'h2 Title')
 							.value
-					: product?.customFields.find(item =>
-							item?.type_name?.includes(' filters')
-					  )
-					? product?.customFields
+					: product?.customFields
 							.find(item => item?.type_name?.includes(' filters'))
-							?.custom_fields.find(item => item.name === 'Product Type')?.value
-					: product?.model}
+							?.custom_fields.find(item => item.name === 'Product Type')?.value}
 			</h3>
 			<ul className='d-flex flex-wrap justify-content-center list-unstyled gap-2'>
 				{Array.isArray(data.products) &&
