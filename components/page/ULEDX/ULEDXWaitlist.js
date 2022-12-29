1e964a408f8c32435b4725ea758bb718135f7d4d
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2022-12-29 10:36:57 +0330

    ULEDX update

--- components/page/ULEDX/ULEDXWaitlist.js
+++ components/page/ULEDX/ULEDXWaitlist.js
@@ -1,20 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import ULEDXWaitlistVideoBox from './ULEDXWaitlistVIdeoBox'
 import ULEDXWaitlistForm from './ULEDXWaitlistForm'
+import { useWindowSize } from 'hooks/useWindowSize'
 
 const ULEDXWaitlist = ({ data }) => {
 	const { structure } = data
+
+	const [videoFullSize, setVideoFullSize] = useState(false)
+	const windowSize = useWindowSize()
+
+	const fullSizeVideo = () => {
+		if (windowSize[0] >= 768) setVideoFullSize(prevState => !prevState)
+		else setVideoFullSize(false)
+	}
 
 	return (
 		<section>
 			<div className='uledx_waitlist'>
 				<div className='row mx-0'>
-					<div className='col-12 col-md-7 px-0'>
-						<ULEDXWaitlistVideoBox data={structure?.video} />
+					<div className={`col-12 ${videoFullSize ? '' : 'col-md-7'} px-0`}>
+						<ULEDXWaitlistVideoBox
+							data={structure?.video}
+							videoSizeHandler={fullSizeVideo}
+						/>
 					</div>
-					<div className='col-12 col-md-5 px-0'>
-						<ULEDXWaitlistForm data={structure} />
-					</div>
+					{!videoFullSize && (
+						<div className='col-12 col-md-5 px-0'>
+							<ULEDXWaitlistForm data={structure} />
+						</div>
+					)}
 				</div>
 			</div>
 		</section>
---
+++ utils/components/ULEDXComponents.js
@@ -0,0 +1,14 @@
+module.exports.ULEDXComponents = [
+	{
+		name: 'ULEDXHome',
+		path: 'components/page/ULEDX/ULEDXHome'
+	},
+	{
+		name: 'ULEDXWaitlist',
+		path: 'components/page/ULEDX/ULEDXWaitlist'
+	},
+	{
+		name: 'ULEDXGetNotified',
+		path: 'components/page/ULEDX/ULEDXGetNotified'
+	}
+]
