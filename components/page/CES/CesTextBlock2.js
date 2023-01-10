921e0fa542de72d63cd24619cc8913410546264d
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-01-10 19:05:45 +0330

    some changes in text block 2

--- components/page/CES/CesTextBlock2.js
+++ components/page/CES/CesTextBlock2.js
@@ -10,43 +10,51 @@ const CesTexBlock2 = ({ data }) => {
 
 	return (
 		<section>
-			<div className='ces_cards'>
+			<div className='ces_cards text_block_2'>
 				<div className='custom_container text-center'>
-					<div className='title large'>
+					<div className='title large' style={{ marginBottom: '150px' }}>
 						<h2
 							dangerouslySetInnerHTML={{ __html: content?.title?.value }}></h2>
 					</div>
 					<div className='row align-items-start flex-wrap flex-md-nowrap'>
 						<div className='col-12 col-md-7 text-start mb-10 mb-md-0'>
-							<h3 className='fs-1 fw-bolder-700 text-white mb-10	'>
-								<span className='fs-2qx'>{content?.subtitle?.value}</span>
+							<h3 className='fw-bolder-700 text-white mb-13	'>
+								<span className='fs-2hx '>{content?.subtitle?.value}</span>
 							</h3>
 							<div
-								className='text-white mb-20'
+								className='description mb-10'
 								dangerouslySetInnerHTML={{
 									__html: content?.description?.value
 								}}></div>
-							{content?.link?.value ? (
-								<Link href={content?.link?.value}>
-									<a style={{ color: 'rgb(0, 204, 204)', fontSize: '18px' }}>
-										{content?.link?.title}
+							{content?.link2?.value ? (
+								<Link href={content?.link2?.value}>
+									<a className='n-btn primary-text primary-hover'>
+										{content?.link2?.title} {'>'}
 									</a>
 								</Link>
 							) : null}
 						</div>
 						<div className='col-12 col-md-5 text-start'>
 							<h5 className='text-white fs-2 mb-10'>
-								<span style={{ fontSize: '18px' }}>
-									{content?.specs?.value}
-								</span>
+								<span>{content?.specs?.value}</span>
 							</h5>
 							<ul style={{ lineHeight: '27px' }}>
 								{content?.list?.value.map((item, index) => (
-									<li key={index} className='text-white'>
+									<li
+										key={index}
+										style={{ fontSize: '18px' }}
+										className='text-white'>
 										{item?.title?.value}
 									</li>
 								))}
 							</ul>
+							{content?.link1?.value ? (
+								<Link href={content?.link1?.value}>
+									<a className='n-btn primary-text primary-hover text-decoration-none'>
+										{content?.link1?.title} {'>'}
+									</a>
+								</Link>
+							) : null}
 						</div>
 					</div>
 				</div>
