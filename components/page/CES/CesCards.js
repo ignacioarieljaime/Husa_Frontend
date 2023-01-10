e44e4269b4b1e81980e0f359b40e88ce4178c6cd
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-01-10 14:31:58 +0330

    ces cards

--- components/page/CES/CesCards.js
+++ components/page/CES/CesCards.js
@@ -15,279 +15,171 @@ const CesCards = ({ data }) => {
 					<div
 						className='title'
 						dangerouslySetInnerHTML={{ __html: content?.title?.value }}></div>
-					<div className='row justify-content-center align-items-center boxes px-3'>
-						<div className='col-12 px-0'>
+					<div className='row justify-content-between align-items-center boxes px-3'>
+						{content?.block1?.value?.status?.value === 'active' ? (
+							<div className='col-12 px-0'>
+								<div
+									className='tv-item full-size'
+									style={{
+										backgroundImage: `url(${content?.block1?.value?.backgroundImage?.src})`
+									}}>
+									<div className='content'>
+										<img
+											src={content?.block1?.value?.image1?.src}
+											alt={content?.block1?.value?.image1?.alt}
+											className='logo'
+										/>
+										<img
+											src={content?.block1?.value?.image2?.src}
+											alt={content?.block1?.value?.image2?.alt}
+											className='image'
+										/>
+										<div
+											className={`title ${
+												content?.block1?.value?.theme?.value === 'light'
+													? 'text-white'
+													: ''
+											}`}
+											dangerouslySetInnerHTML={{
+												__html: content?.block1?.value?.title?.value
+											}}></div>
+										{content?.block1?.value?.link?.value ? (
+											<Link href={content?.block1?.value?.link?.value}>
+												<a
+													className={`n-btn p-4 transparent d-block w-fit ${
+														content?.block1?.value?.theme?.value === 'light'
+															? 'outline-white'
+															: 'outline-black'
+													}`}>
+													{content?.block1?.value?.link?.title}
+												</a>
+											</Link>
+										) : null}
+									</div>
+								</div>
+							</div>
+						) : null}
+						{content?.block2?.value?.status?.value === 'active' ? (
+							<div className='col-12 px-0'>
+								<div
+									className='tv-item full-size block2'
+									style={{
+										backgroundImage: `url(${content?.block2?.value?.backgroundImage?.src})`
+									}}>
+									<div className='content'>
+										<div
+											className={`title ${
+												content?.block2?.value?.theme?.value === 'light'
+													? 'text-white'
+													: ''
+											}`}
+											dangerouslySetInnerHTML={{
+												__html: content?.block2?.value?.title?.value
+											}}></div>
+										{content?.block2?.value?.link?.value ? (
+											<Link href={content?.block2?.value?.link?.value}>
+												<a
+													className={`n-btn p-4 transparent d-block w-fit ms-auto ${
+														content?.block2?.value?.theme?.value === 'light'
+															? 'outline-white'
+															: 'outline-black'
+													}`}>
+													{content?.block2?.value?.link?.title}
+												</a>
+											</Link>
+										) : null}
+									</div>
+								</div>
+							</div>
+						) : null}
+						{content?.list?.value.map((item, index) => (
 							<div
-								className='tv-item full-size block1'
-								style={{
-									backgroundImage: `url(${content?.block1?.value?.backgroundImage?.src})`
-								}}>
-								<div className='content'>
-									<div
-										className={`title ${
-											content?.block1?.value?.theme?.value === 'light'
-												? 'text-white'
-												: ''
-										}`}
-										dangerouslySetInnerHTML={{
-											__html: content?.block1?.value?.title?.value
-										}}></div>
-									{content?.block1?.value?.link?.value ? (
-										<Link href={content?.block1?.value?.link?.value}>
-											<a
-												className={`n-btn transparent d-block w-fit ${
-													content?.block1?.value?.theme?.value === 'light'
-														? 'outline-white'
-														: 'outline-black'
-												}`}>
-												{content?.block1?.value?.link?.title}
-											</a>
-										</Link>
-									) : null}
-								</div>
-							</div>
-						</div>
-						<div className='col-12 px-0'>
-							<div
-								className='tv-item full-size'
-								style={{
-									backgroundImage: `url(${content?.block2?.value?.backgroundImage?.src})`
-								}}>
-								<div className='content'>
-									<img
-										src={content?.block2?.value?.image1?.src}
-										alt={content?.block2?.value?.image1?.alt}
-										className='logo'
+								key={index}
+								className={`box flex-column d-flex col-12 col-md-6`}>
+								<div className='box-background'>
+									<CustomImage
+										src={item?.backgroundImage?.src}
+										alt={item?.backgroundImage?.alt}
+										wrapperHeight='100%'
+										wrapperWidth='100%'
 									/>
-									<img
-										src={content?.block2?.value?.image2?.src}
-										alt={content?.block2?.value?.image2?.alt}
-										className='image'
-									/>
-									<div
-										className={`title ${
-											content?.block2?.value?.theme?.value === 'light'
-												? 'text-white'
-												: ''
-										}`}
-										dangerouslySetInnerHTML={{
-											__html: content?.block2?.value?.title?.value
-										}}></div>
-									{content?.block2?.value?.link?.value ? (
-										<Link href={content?.block2?.value?.link?.value}>
-											<a
-												className={`n-btn transparent d-block w-fit ${
-													content?.block2?.value?.theme?.value === 'light'
-														? 'outline-white'
-														: 'outline-black'
-												}`}>
-												{content?.block2?.value?.link?.title}
-											</a>
-										</Link>
-									) : null}
+									<div className='box-background-shade'></div>
+								</div>
+								<div className='box-content'>
+									<div className='d-flex justify-content-between align-items-center w-100'>
+										{item?.image?.src && (
+											<img
+												src={item?.image?.src}
+												alt={item?.image?.alt}
+												width='100%'
+												className='mb-5 mb-md-7'
+											/>
+										)}
+										<h3
+											className={`mb-3 mb-md-0 title ${
+												item?.theme?.value === 'light' ? 'text-white' : ''
+											}`}>
+											{item?.title?.value}
+										</h3>
+										{item?.link?.title && item?.link?.value ? (
+											<Link href={item?.link?.value ? item?.link?.value : '/'}>
+												<a
+													className={`n-btn p-4 transparent d-block w-fit ${
+														item?.theme?.value === 'light'
+															? 'outline-white'
+															: 'outline-black'
+													}`}>
+													{item?.link?.title}
+												</a>
+											</Link>
+										) : null}
+									</div>
 								</div>
 							</div>
-						</div>
-						{/* block3 */}
-						<div className={`box flex-column d-flex col-12 col-md-6`}>
-							<div className='box-background'>
-								<CustomImage
-									src={content?.block3?.value?.backgroundImage?.src}
-									alt={content?.block3?.value?.backgroundImage?.alt}
-									wrapperHeight='100%'
-									wrapperWidth='100%'
-								/>
-								<div className='box-background-shade'></div>
-							</div>
-							<div className='box-content'>
-								<div>
-									{content?.block3?.value?.image?.src && (
+						))}
+						{content?.block3?.value?.status?.value === 'active' ? (
+							<div className='col-12 px-0'>
+								<div
+									className='tv-item full-size'
+									style={{
+										backgroundImage: `url(${content?.block3?.value?.backgroundImage?.src})`
+									}}>
+									<div className='content'>
 										<img
-											src={content?.block3?.value?.image?.src}
-											alt={content?.block3?.value?.image?.alt}
-											width='100%'
-											className='mb-5 mb-md-7'
+											src={content?.block3?.value?.image1?.src}
+											alt={content?.block3?.value?.image1?.alt}
+											className='logo'
 										/>
-									)}
-									<h3
-										className={`mb-3 mb-md-6 title ${
-											content?.block3?.value?.theme?.value === 'light'
-												? 'text-white'
-												: ''
-										}`}>
-										{content?.block3?.value?.title?.value}
-									</h3>
-									{content?.block3?.value?.link?.title &&
-									content?.block3?.value?.link?.value ? (
-										<Link
-											href={
-												content?.block3?.value?.link?.value
-													? content?.block3?.value?.link?.value
-													: '/'
-											}>
-											<a
-												className={`n-btn transparent d-block w-fit ${
-													content?.block2?.value?.theme?.value === 'light'
-														? 'outline-white'
-														: 'outline-black'
-												}`}>
-												{content?.block3?.value?.link?.title}
-											</a>
-										</Link>
-									) : null}
-								</div>
-							</div>
-						</div>
-						{/* block4 */}
-						<div className={`box flex-column d-flex col-12 col-md-6`}>
-							<div className='box-background'>
-								<CustomImage
-									src={content?.block4?.value?.backgroundImage?.src}
-									alt={content?.block4?.value?.backgroundImage?.alt}
-									wrapperHeight='100%'
-									wrapperWidth='100%'
-								/>
-								<div className='box-background-shade'></div>
-							</div>
-							<div className='box-content'>
-								<div>
-									{content?.block4?.value?.image?.src && (
 										<img
-											src={content?.block4?.value?.image?.src}
-											alt={content?.block4?.value?.image?.alt}
-											width='100%'
-											className='mb-5 mb-md-7'
+											src={content?.block3?.value?.image2?.src}
+											alt={content?.block3?.value?.image2?.alt}
+											className='image'
 										/>
-									)}
-									<h3
-										className={`mb-3 mb-md-6 title ${
-											content?.block4?.value?.theme?.value === 'light'
-												? 'text-white'
-												: ''
-										}`}>
-										{content?.block4?.value?.title?.value}
-									</h3>
-									{content?.block4?.value?.link?.title &&
-									content?.block4?.value?.link?.value ? (
-										<Link
-											href={
-												content?.block4?.value?.link?.value
-													? content?.block4?.value?.link?.value
-													: '/'
-											}>
-											<a
-												className={`n-btn transparent d-block w-fit ${
-													content?.block2?.value?.theme?.value === 'light'
-														? 'outline-white'
-														: 'outline-black'
-												}`}>
-												{content?.block4?.value?.link?.title}
-											</a>
-										</Link>
-									) : null}
+										<div
+											className={`title ${
+												content?.block3?.value?.theme?.value === 'light'
+													? 'text-white'
+													: ''
+											}`}
+											dangerouslySetInnerHTML={{
+												__html: content?.block3?.value?.title?.value
+											}}></div>
+										{content?.block3?.value?.link?.value ? (
+											<Link href={content?.block3?.value?.link?.value}>
+												<a
+													className={`n-btn p-4 transparent d-block w-fit ${
+														content?.block3?.value?.theme?.value === 'light'
+															? 'outline-white'
+															: 'outline-black'
+													}`}>
+													{content?.block3?.value?.link?.title}
+												</a>
+											</Link>
+										) : null}
+									</div>
 								</div>
 							</div>
-						</div>
-						{/* block5 */}
-						<div className={`box flex-column d-flex col-12 col-md-6`}>
-							<div className='box-background'>
-								<CustomImage
-									src={content?.block5?.value?.backgroundImage?.src}
-									alt={content?.block5?.value?.backgroundImage?.alt}
-									wrapperHeight='100%'
-									wrapperWidth='100%'
-								/>
-								<div className='box-background-shade'></div>
-							</div>
-							<div className='box-content'>
-								<div>
-									{content?.block5?.value?.image?.src && (
-										<img
-											src={content?.block5?.value?.image?.src}
-											alt={content?.block5?.value?.image?.alt}
-											width='100%'
-											className='mb-5 mb-md-7'
-										/>
-									)}
-									<h3
-										className={`mb-3 mb-md-6 title ${
-											content?.block5?.value?.theme?.value === 'light'
-												? 'text-white'
-												: ''
-										}`}>
-										{content?.block5?.value?.title?.value}
-									</h3>
-									{content?.block5?.value?.link?.title &&
-									content?.block5?.value?.link?.value ? (
-										<Link
-											href={
-												content?.block5?.value?.link?.value
-													? content?.block5?.value?.link?.value
-													: '/'
-											}>
-											<a
-												className={`n-btn transparent d-block w-fit ${
-													content?.block2?.value?.theme?.value === 'light'
-														? 'outline-white'
-														: 'outline-black'
-												}`}>
-												{content?.block5?.value?.link?.title}
-											</a>
-										</Link>
-									) : null}
-								</div>
-							</div>
-						</div>
-						{/* block6 */}
-						<div className={`box flex-column d-flex col-12 col-md-6`}>
-							<div className='box-background'>
-								<CustomImage
-									src={content?.block6?.value?.backgroundImage?.src}
-									alt={content?.block6?.value?.backgroundImage?.alt}
-									wrapperHeight='100%'
-									wrapperWidth='100%'
-								/>
-								<div className='box-background-shade'></div>
-							</div>
-							<div className='box-content'>
-								<div>
-									{content?.block6?.value?.image?.src && (
-										<img
-											src={content?.block6?.value?.image?.src}
-											alt={content?.block6?.value?.image?.alt}
-											width='100%'
-											className='mb-5 mb-md-7'
-										/>
-									)}
-									<h3
-										className={`mb-3 mb-md-6 title ${
-											content?.block6?.value?.theme?.value === 'light'
-												? 'text-white'
-												: ''
-										}`}>
-										{content?.block6?.value?.title?.value}
-									</h3>
-									{content?.block6?.value?.link?.title &&
-									content?.block6?.value?.link?.value ? (
-										<Link
-											href={
-												content?.block6?.value?.link?.value
-													? content?.block6?.value?.link?.value
-													: '/'
-											}>
-											<a
-												className={`n-btn transparent d-block w-fit ${
-													content?.block2?.value?.theme?.value === 'light'
-														? 'outline-white'
-														: 'outline-black'
-												}`}>
-												{content?.block6?.value?.link?.title}
-											</a>
-										</Link>
-									) : null}
-								</div>
-							</div>
-						</div>
+						) : null}
 					</div>
 				</div>
 			</div>
--- components/page/CES/CesHero.js
+++ components/page/CES/CesHero.js
@@ -1,39 +1,13 @@
 import React from 'react'
 import CustomImage from 'components/common/CustomImage'
 import Link from 'next/link'
-import {
-	useParallax,
-	useParallaxController,
-	ParallaxProvider
-} from 'react-scroll-parallax'
-import { useWindowSize } from 'hooks/useWindowSize'
 
-const CesHero = ({ data: { structure } }) => {
-	return (
-		<ParallaxProvider>
-			<CesHeroContainer structure={structure} />
-		</ParallaxProvider>
-	)
-}
-
-const CesHeroContainer = ({ structure }) => {
-	const parallaxController = useParallaxController()
-	const windowSize = useWindowSize()
-
-	const blockRef = useParallax({
-		speed: -5,
-		shouldAlwaysCompleteAnimation: true,
-		disabled: windowSize[0] < 768 ? true : false,
-		scale: [1, 0.5],
-		translateY: [0, 0]
-	})
-
+const CesHero = ({ structure }) => {
 	return (
 		<section>
 			<div className='ces_hero'>
-				<div className='custom_container' ref={blockRef.ref}>
+				<div className='custom_container'>
 					<CustomImage
-						onLoad={() => parallaxController.update()}
 						src={structure?.backgroundImage?.src}
 						alt={'parallax background'}
 						className={'img-fluid'}
@@ -41,27 +15,34 @@ const CesHeroContainer = ({ structure }) => {
 						wrapperWidth={'100%'}
 					/>
 					<div className='banner-content'>
-						<h1
-							className='title fs-3x'
-							dangerouslySetInnerHTML={{
-								__html: structure?.title?.value
-							}}></h1>
+						{structure?.title?.value ? (
+							<h1
+								className='title fs-3x'
+								dangerouslySetInnerHTML={{
+									__html: structure?.title?.value
+								}}></h1>
+						) : null}
 						<div>
-							{structure?.link?.value ? (
+							{structure?.link1?.value ? (
 								<Link
-									href={structure?.link?.value ? structure?.link?.value : '/'}>
+									href={
+										structure?.link1?.value ? structure?.link1?.value : '/'
+									}>
 									<a className='n-btn outline-white transparent fs-4'>
-										{structure?.link?.title}
+										{structure?.link1?.title}
 									</a>
 								</Link>
 							) : null}
-							<Link
-								href={structure?.link?.value ? structure?.link?.value : '/'}>
-								<a className='n-btn white-text ms-2'>
-									{structure?.link?.title}
-									{'>'}
-								</a>
-							</Link>
+							{structure?.link2?.value ? (
+								<Link
+									href={
+										structure?.link2?.value ? structure?.link2?.value : '/'
+									}>
+									<a className='n-btn white-text ms-2'>
+										{structure?.link2?.title} {'>'}
+									</a>
+								</Link>
+							) : null}
 						</div>
 					</div>
 				</div>
