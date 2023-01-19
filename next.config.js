bdce9dd78abf0085ef1a114fec9edaf3a9b0d40d
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-01-19 23:34:26 +0330

    fix config

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const moduleExports = {
 		NEXT_PUBLIC_BRAND_ID: process.env.BRAND_ID || 3,
 		NEXT_PUBLIC_GTM_ID: process.env.GTM_ID || 'GTM-WBPC7RT',
 		NEXT_PUBLIC_VELARO_ID: process.env.VELARO_ID || '30263',
-		NEXT_PUBLIC_APP_LOCATION: process.env.APP_LOCATION || 'production'
+		NEXT_PUBLIC_APP_LOCATION: process.env.APP_LOCATION || 'stage'
 	},
 
 	images: {
