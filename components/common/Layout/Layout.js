40e49bc6224dc2c6950ccc513e319722477c1f20
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-02-07 15:27:44 +0330

    fix some change

--- components/common/Layout/Layout.js
+++ components/common/Layout/Layout.js
@@ -17,16 +17,7 @@ function Layout({ children, meta, title, header }) {
 	const [showGoTop, setShowGoTop] = useState(false)
 	const router = useRouter()
 	const [showChild, setShowChild] = useState(false)
-	const [comp, setComp] = useState(<></>)
-
-	useEffect(() => {
-		const load = async () => {
-			const velaroGenerator = (await import('../VelaroChat')).default
-			setComp(velaroGenerator)
-		}
-
-		load()
-	}, [])
+	
 
 	useEffect(() => {
 		checkIsAdmin()
@@ -137,7 +128,6 @@ function Layout({ children, meta, title, header }) {
 					<FontAwesomeIcon icon={faChevronUp} />
 				</button>
 			)}
-			<>{comp}</>
 		</>
 	)
 }
--- components/common/VelaroChatBox.js
+++ components/common/VelaroChatBox.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import React, { useState, useEffect } from 'react'
 
-const customCss = '.velaro-custom-launcher-frame{display: inline-block !important}'
 
 export default function VelaroChatBox() {
+const customCss = '.velaro-custom-launcher-frame{visibility: unset !important}'
+
 	return (
 		<>
 			<Head>
