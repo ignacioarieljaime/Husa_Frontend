3f4a456dd8a1015d8493a182ccef962f1fc65ecc
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-02-10 14:23:30 +0330

    fix some change

--- components/common/Header/HamburgerMenuDropdowns.js
+++ components/common/Header/HamburgerMenuDropdowns.js
@@ -6,33 +6,43 @@ const HamburgerMenuDropdowns = ({ columns, asideHandler }) => {
 
 	return (
 		<li className='nav-item dropdown'>
-			{/* <Link href={columns.url ? columns.url : ''}> */}
-			<button
-				className='nav-link dropdown-toggle lh-base p-1 px-3 m-2'
-				role='button'
-				onClick={() => setShow(show => !show)}>
-				<span className='underline-on-hover'>{columns.name}</span>
-				{/* <FontAwesomeIcon icon={faChevronDown} /> */}
-			</button>
-			{/* </Link> */}
-			<div
-				className={`dropdown-menu ps-6 pe-4 ${show ? 'd-block' : 'd-none'}`}
-				aria-labelledby='tv-audio-dropdown'>
-				{columns.columns.map(colum => (
-					<>
-						{colum.map(item => (
-							<Link href={item.url ? item.url : ''}>
-								<a
-									onClick={() => asideHandler(false)}
-									className='dropdown-item'>
-									<span className='underline-on-hover'>{item.name}</span>
-									{/* <FontAwesomeIcon icon={faChevronLeft} size='sm' /> */}
-								</a>
-							</Link>
-						))}
-					</>
-				))}
-			</div>
+			{columns.columns.length === 0 ? (
+				<Link href={columns.url ? columns.url : ''}>
+					<a className='nav-link dropdown-toggle lh-base p-1 px-3 m-2 underline-on-hover'>
+						{columns.name}
+					</a>
+					{/* <FontAwesomeIcon icon={faChevronDown} /> */}
+				</Link>
+			) : (
+				<button
+					className='nav-link dropdown-toggle lh-base p-1 px-3 m-2'
+					role='button'
+					onClick={() => setShow(show => !show)}>
+					<span className='underline-on-hover'>{columns.name}</span>
+					{/* <FontAwesomeIcon icon={faChevronDown} /> */}
+				</button>
+			)}
+
+			{columns.columns.length !== 0 && (
+				<div
+					className={`dropdown-menu ps-6 pe-4 ${show ? 'd-block' : 'd-none'}`}
+					aria-labelledby='tv-audio-dropdown'>
+					{columns.columns.map(colum => (
+						<>
+							{colum.map(item => (
+								<Link href={item.url ? item.url : ''}>
+									<a
+										onClick={() => asideHandler(false)}
+										className='dropdown-item'>
+										<span className='underline-on-hover'>{item.name}</span>
+										{/* <FontAwesomeIcon icon={faChevronLeft} size='sm' /> */}
+									</a>
+								</Link>
+							))}
+						</>
+					))}
+				</div>
+			)}
 		</li>
 	)
 }
--- components/page/Product/ProductSliderLinkButton.js
+++ components/page/Product/ProductSliderLinkButton.js
@@ -6,7 +6,21 @@ function ProductSliderLinkButton({ data, pim }) {
 	const [url] = useState(RouteHandler(data.products[0], 'product'))
 	return (
 		<>
-			{url ? (
+			{pim?.name?.includes(data?.title) ? (
+				<span
+					style={{
+						border: '1px solid #239d98',
+						fontSize: '16px',
+						color: '#239d98',
+						padding: '3px 9px',
+						height: '39px',
+						display: 'inline-flex',
+						alignItems: 'center'
+					}}
+					className={` m-2 rounded-3 m-0 `}>
+					{data.title}
+				</span>
+			) : url ? (
 				<Link href={url}>
 					<a
 						className={`btn  m-2 rounded-3 m-0 ${
