e9cb45c2ae26a3f42237c90c23ca21c43dbb78b1
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-02-17 16:22:11 +0330

    .

--- components/page/LaserTvInstallation/LaserTvInstallationForm.js
+++ components/page/LaserTvInstallation/LaserTvInstallationForm.js
@@ -52,8 +52,8 @@ const LaserTvInstallationForm = ({ data }) => {
 		setErrors(null)
 		try {
 			let response = await axios.post(
-				// 'https://imcrm2.dev-api.hisenseportal.com/api/v1/form/fill/F63eb5ac8e78b1',
-				`${process.env.NEXT_PUBLIC_CRM_API_ROUTE}/F63eb5ac8e78b1`,
+				'https://imcrm2.dev-api.hisenseportal.com/api/v1/form/fill/F63eb5ac8e78b1',
+				// `${process.env.NEXT_PUBLIC_CRM_API_ROUTE}/F63eb5ac8e78b1`,
 				formBody
 			)
 			if (response.data.userForm.id && response.data.userForm.created_at) {
