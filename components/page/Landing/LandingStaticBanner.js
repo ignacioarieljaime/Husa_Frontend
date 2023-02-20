e9435da871af686afd3a99e94898a19c7610cb38
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-02-20 17:31:09 +0330

    LandingStaticBanner

--- components/page/Landing/LandingStaticBanner.js
+++ components/page/Landing/LandingStaticBanner.js
@@ -8,19 +8,31 @@ const LandingStaticBanner = ({ data }) => {
 		<section>
 			<div className='landing_new_banner'>
 				<div className='background'>
-					<CustomImage
-						src={structure?.image?.src}
-						alt={structure?.image?.alt}
-						wrapperHeight='100%'
-						wrapperWidth='100%'
-						wrapperClass='image'
-					/>
+					{structure?.image?.src ? (
+						<CustomImage
+							src={structure?.image?.src}
+							alt={structure?.image?.alt}
+							wrapperHeight='100%'
+							wrapperWidth='100%'
+							wrapperClass='insider'
+						/>
+					) : structure?.video?.value ? (
+						<video
+							autoPlay={true}
+							muted={true}
+							loop={true}
+							playsinline={true}
+							width='100%'
+							height='100%'>
+							<source src={structure?.video?.value} data-wf-ignore='true' />
+						</video>
+					) : null}
 				</div>
 				<div className='content'>
 					{structure?.link?.value ? (
 						<Link href={structure?.link?.value}>
 							<a
-								className={`n-btn py-3 px-6 ${
+								className={`n-btn py-3 px-6 transparent ${
 									structure?.theme?.value === 'dark'
 										? 'outline-black'
 										: 'outline-white'
