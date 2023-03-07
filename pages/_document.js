d62af01689acaad7422c1a2ea87c9f02ee0d2197
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-03-07 09:50:38 +0330

    fix GTM

--- pages/_document.js
+++ pages/_document.js
@@ -48,6 +48,11 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   gtag('js', new Date());
 
   gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}</Script> */}
+				<noscript
+					dangerouslySetInnerHTML={{
+						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+					}}></noscript>
 			</body>
 		</Html>
 	)
