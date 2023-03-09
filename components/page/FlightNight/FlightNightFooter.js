a24cb8b5723968474d3074427d67f20d86d73f6e
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-03-09 18:51:11 +0330

    new components

--- components/page/FlightNight/FlightNightFooter.js
+++ components/page/FlightNight/FlightNightFooter.js
@@ -1,7 +1,20 @@
+import CustomImage from 'components/common/CustomImage'
 import React from 'react'
 
 const FlightNightFooter = () => {
-	return <div></div>
+	return (
+		<footer>
+			<div className='bg-black py-4'>
+				<CustomImage
+					wrapperWidth='100%'
+					WrapperMaxWidth='100px'
+					wrapperHeight='auto'
+					wrapperClass='mx-auto'
+					src='http://localhost:3000/_next/image?url=https%3A%2F%2Fwww.hisense-usa.com%2F_next%2Fimage%3Furl%3Dhttps%253A%252F%252Ffiles.hisense-usa.com%252Fstorage%252Fhisense%252Fasset%252Fimages%252F663efed935688d.webp%26w%3D1920%26q%3D75&w=1920&q=75'
+				/>
+			</div>
+		</footer>
+	)
 }
 
 export default FlightNightFooter
