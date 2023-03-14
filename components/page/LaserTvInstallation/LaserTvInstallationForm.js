d746c46c6d9dd9244ce8389c90da33a574a5527c
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-03-14 13:25:20 +0330

    wip

--- components/page/LaserTvInstallation/LaserTvInstallationForm.js
+++ components/page/LaserTvInstallation/LaserTvInstallationForm.js
@@ -96,11 +96,11 @@ const LaserTvInstallationForm = ({ data }) => {
 						dispatch={dispatch}
 						errors={errors}
 					/> */}
-					{/* <InstallationInfoForm
+					<InstallationInfoForm
 						data={formBody}
 						dispatch={dispatch}
 						errors={errors}
-					/> */}
+					/>
 					<div className='text-center mb-0 mb-md-15 m-15'>
 						<button className='n-btn primary py-4 px-6 mb-7'>Submit</button>
 						{errors ? (
