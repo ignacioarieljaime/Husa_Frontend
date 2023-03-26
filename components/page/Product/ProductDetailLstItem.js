0d1cda9f9b8701f8a369852bef679a3e972f37f0
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-03-26 12:31:29 +0330

    updated specs

--- components/page/Product/ProductDetailLstItem.js
+++ components/page/Product/ProductDetailLstItem.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 function ProductDetailLstItem({ title, value, theme }) {
 	return (
-		<>
+		<dd className='col-12 col-sm-6 col-md-3 mb-5 p-1'>
 			<dt>{title}</dt>
-			<dd className={`col-6 ${theme === "light" ? "text-black spec-after-style":`text-white`} col-md-3 mb-5`} data-label={title}>
+			<dd
+				className={`d_desc ${
+					theme === 'light' ? 'text-black spec-after-style' : `text-white`
+				} `}
+				data-label={title}>
 				{value}
 			</dd>
-		</>
+		</dd>
 	)
 }
 
