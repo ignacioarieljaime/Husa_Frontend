19f8990ab78b23ea3c4554b9339244b632d433fa
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-04-06 15:15:06 +0430

    remove locality datalayer

--- components/page/Product/ChannelAdvisorLocally.js
+++ components/page/Product/ChannelAdvisorLocally.js
@@ -183,32 +183,32 @@ const ChannelAdvisorLocally = ({ model, productData }) => {
 									retailer => retailer.DisplayName === item.RetailerName
 								)?.ProductLink && (
 									<a
-										onClick={() =>
-											window.dataLayer.push({
-												event: 'view_product',
-												eventData: {
-													retailer: item?.RetailerName,
-													productType: productData?.category?.name,
-													productTitle: productData?.name,
-													modal: productData?.model,
-													size: productData?.customFields?.find(
-														item => item.type_name === 'TV filters'
-													)
-														? productData?.customFields
-																?.find(item => item.type_name === 'TV filters')
-																?.custom_fields.find(
-																	item => item.name === 'Size class'
-																)?.value
-														: productData?.custom_fields?.find(
-																item => item.title === 'Size class'
-														  )
-														? productData?.custom_fields?.find(
-																item => item.title === 'Size class'
-														  )?.value
-														: ''
-												}
-											})
-										}
+										// onClick={() =>
+										// 	window.dataLayer.push({
+										// 		event: 'view_product',
+										// 		eventData: {
+										// 			retailer: item?.RetailerName,
+										// 			productType: productData?.category?.name,
+										// 			productTitle: productData?.name,
+										// 			modal: productData?.model,
+										// 			size: productData?.customFields?.find(
+										// 				item => item.type_name === 'TV filters'
+										// 			)
+										// 				? productData?.customFields
+										// 						?.find(item => item.type_name === 'TV filters')
+										// 						?.custom_fields.find(
+										// 							item => item.name === 'Size class'
+										// 						)?.value
+										// 				: productData?.custom_fields?.find(
+										// 						item => item.title === 'Size class'
+										// 				  )
+										// 				? productData?.custom_fields?.find(
+										// 						item => item.title === 'Size class'
+										// 				  )?.value
+										// 				: ''
+										// 		}
+										// 	})
+										// }
 										href={
 											onlineRetailer.find(
 												retailer => retailer.DisplayName === item.RetailerName
