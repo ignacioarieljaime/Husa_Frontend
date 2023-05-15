4f0afda959f0c415549e68a49b92a94ebbd6017e
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-05-15 12:11:10 +0330

    dynamic some component

--- components/page/NewsRoom/NewsRoomMedia.js
+++ components/page/NewsRoom/NewsRoomMedia.js
@@ -1,45 +1,61 @@
 import CustomImage from 'components/common/CustomImage'
-import DownloadIconV2 from 'components/icons/DownloadIconV2'
 import Link from 'next/link'
 import React from 'react'
 
-const NewsRoomMedia = () => {
+const NewsRoomMedia = ({ data }) => {
+	let { structure } = data
 	return (
 		<div className='news_room_media'>
-			<h5>Hisense Media+</h5>
+			<h5>{structure?.title?.value}</h5>
 
 			<div>
+				{structure?.list?.value[0] && (
+					<div>
+						<div>
+							<CustomImage
+								src={structure?.list?.value[0]?.image?.src}
+								wrapperWidth={'100%'}
+								wrapperHeight={'244px'}
+							/>
+							{structure?.list?.value[0]?.title?.value && (
+								<span className='text'>
+									{structure?.list?.value[0]?.title?.value}
+								</span>
+							)}
+						</div>
+						<div>
+							<Link href={structure?.list?.value[0]?.link?.value || '/'}>
+								<a>
+									{structure?.list?.value[0]?.link?.title}
+									{structure?.list?.value[0]?.downloadImage?.src && (
+										<CustomImage
+											src={structure?.list?.value[0]?.downloadImage?.src}
+											wrapperWidth={'16px'}
+											wrapperHeight={'16px'}
+										/>
+									)}
+								</a>
+							</Link>
+						</div>
+					</div>
+				)}
+
 				<div>
 					<div>
 						<CustomImage
-							src={
-								'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=996&t=st=1683373900~exp=1683374500~hmac=6a8571256f2eecbb9073eff715c3ed673f2b350487c29f3771555e1320f6c9fe'
-							}
+							src={structure?.list?.value[1]?.image?.src}
 							wrapperWidth={'100%'}
 							wrapperHeight={'244px'}
 						/>
+						{structure?.list?.value[1]?.title?.value && (
+							<span className='text'>
+								{structure?.list?.value[1]?.title?.value}
+							</span>
+						)}
 					</div>
 					<div>
-						{' '}
-						<a>
-							Download Media Kit <DownloadIconV2 />
-						</a>
-					</div>
-				</div>
-				<div>
-					<div>
-						<CustomImage
-							src={
-								'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=996&t=st=1683373900~exp=1683374500~hmac=6a8571256f2eecbb9073eff715c3ed673f2b350487c29f3771555e1320f6c9fe'
-							}
-							wrapperWidth={'100%'}
-							wrapperHeight={'244px'}
-						/>
-						<span className='text'>Blogsense</span>
-					</div>
-					<div>
-						<Link href={'/'}>
-							<a>Read Blogs</a>
+						<Link href={structure?.list?.value[1]?.link?.value || '/'}>
+							<a>{structure?.list?.value[1]?.link?.title}</a>
 						</Link>
 					</div>
 				</div>
--- components/page/NewsRoom/NewsRoomSlider.js
+++ components/page/NewsRoom/NewsRoomSlider.js
@@ -6,35 +6,38 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 // Import Swiper styles
 import 'swiper/css'
 import 'swiper/css/pagination'
-import "swiper/css/navigation";
+import 'swiper/css/navigation'
 
 // import required modules
-import { Pagination, Navigation } from "swiper";
+import { Pagination, Navigation } from 'swiper'
 import Link from 'next/link'
 
-const NewsRoomSlider = () => {
+const NewsRoomSlider = ({ data }) => {
+	let { structure } = data
 	return (
 		<div className='news_room_slider'>
-			<h3>Newsroom</h3>
+			<h3>{structure?.title?.value}</h3>
 			<Swiper
 				slidesPerView={'auto'}
 				spaceBetween={30}
 				loop
-                navigation={true}
+				navigation={true}
 				centeredSlides={true}
 				pagination={{
 					clickable: true
 				}}
-		        modules={[Pagination, Navigation]}
+				modules={[Pagination, Navigation]}
 				className='news_room_slider_box'>
-				{[1, 1, 2, 1].map(item => (
-					<SwiperSlide>
-						<div className='slider_item'>
+				{structure?.list?.value?.map((item, index) => (
+					<SwiperSlide key={'slider-item-' + index}>
+						<div
+							style={{ backgroundImage: `url(${item?.image?.src})` }}
+							className='slider_item'>
 							<div>
-								<h5>Hisense TV Annoucement</h5>
-								<h6>Hisense TV Annoucement</h6>
-								<Link href={'/'}>
-									<a>Read More</a>
+								<h5>{item?.title?.value}</h5>
+								<h6>{item?.subtitle?.value}</h6>
+								<Link href={item?.btn?.value}>
+									<a>{item?.btn?.title}</a>
 								</Link>
 							</div>
 						</div>
