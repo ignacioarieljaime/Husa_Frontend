30edc2f2dc2cbf517920e03bc50d991c2983a05b
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-06-21 12:31:07 +0330

    careers

--- components/page/Careers/CareersTextBox.js
+++ components/page/Careers/CareersTextBox.js
@@ -1,16 +1,54 @@
 import React, { useState, useEffect } from 'react'
 import CustomImage from 'components/common/CustomImage'
 
-const CareersBanner = ({ data: { structure } }) => {
+const CareersTextBox = ({ data: { structure } }) => {
 	const [text, setText] = useState(null)
 	useEffect(() => {
-		setText(structure?.title?.value)
+		setText(structure?.text?.value)
 	}, [])
-	return <section>
-		<div className="careers_textbox">
-			{structure?.template?.value}
-		</div>
-	</section>
+	return (
+		<section>
+			<div
+				className='careers_textbox'
+				style={{
+					backgroundColor:
+						structure?.template?.value === 'v1'
+							? structure?.backgroundColor?.value
+							: '#fff'
+				}}>
+				{structure?.template?.value === 'v1' ? (
+					<>
+						<div
+							className='v1'
+							style={{ backgroundColor: structure?.backgroundColor?.value }}>
+							<CustomImage
+								src={structure?.image?.src}
+								alt={structure?.image?.alt}
+								wrapperWidth='100%'
+								WrapperMaxWidth='40px'
+								wrapperClass='mb-9 mx-auto'
+							/>
+							<div
+								className='text'
+								dangerouslySetInnerHTML={{
+									__html: text
+								}}></div>
+						</div>
+					</>
+				) : (
+					<>
+						<div className='v2'>
+							<div
+								className='text'
+								dangerouslySetInnerHTML={{
+									__html: text
+								}}></div>
+						</div>
+					</>
+				)}
+			</div>
+		</section>
+	)
 }
 
-export default CareersBanner
+export default CareersTextBox
