2a9f2b628accc4a828c087bd36ac08d92ca9f5df
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-06-22 12:15:25 +0330

    QA

--- components/page/Careers/CareersAccordionItemSublist.js
+++ components/page/Careers/CareersAccordionItemSublist.js
@@ -4,7 +4,7 @@ import React from 'react'
 const CareersAccordionItemSublist = ({ data, collapsed }) => {
 	return (
 		<div className='careers_accordion_content row mx-0'>
-			<div className='col-12 col-md-8 mb-4 px-0'>
+			<div className='col-12 col-md-8 px-0'>
 				<h4 className='faq-accordion-question mb-8'>{data?.title?.value}</h4>
 				<div
 					className='faq-accordion-answer'
