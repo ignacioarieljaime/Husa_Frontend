084413cda4f77b403bf8eb4a55d63bec90c25943
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-06-26 09:44:06 +0330

    chnage register text

--- components/page/ProductSupport/ProductSupportRegisterTab.js
+++ components/page/ProductSupport/ProductSupportRegisterTab.js
@@ -340,7 +340,8 @@ const ProductSupportRegisterTab = ({ pim, data }) => {
 									id='form-checkbox-input'
 									className='d-none'
 								/>
-								Sign me up for future news
+								Sign up for special deals, news, and important product
+								information.
 							</label>
 						</div>
 						<div className='col-12 text-center my-9'>
