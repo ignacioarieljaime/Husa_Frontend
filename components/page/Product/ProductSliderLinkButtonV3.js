f5f2636a7933c210c94bac8bfe19e2ad55923a6a
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-07-20 11:57:48 +0330

    fixed all product info boxes

--- components/page/Product/ProductSliderLinkButtonV3.js
+++ components/page/Product/ProductSliderLinkButtonV3.js
@@ -2,16 +2,18 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import { RouteHandler } from 'utils/routeHandler'
 
-function ProductSliderLinkButtonV3({ data, pim }) {
+function ProductSliderLinkButtonV3({ data, pim, theme }) {
 	const [url] = useState(RouteHandler(data.products[0], 'product'))
 	return (
 		<>
 			{pim?.name?.includes(data?.title) ? (
-				<span className={`sizes_item`}>{data.title}</span>
+				<span className={`sizes_item ${theme === 'light' ? 'white' : 'dark'}`}>
+					{data.title}
+				</span>
 			) : url ? (
 				<Link href={url}>
 					<a
-						className={`sizes_item ${
+						className={`sizes_item ${theme === 'light' ? 'white' : 'dark'} ${
 							pim?.custom_fields[2]?.value === data.title
 								? 'btn-primary-new text-white'
 								: 'btn-outline-dark'
@@ -22,7 +24,7 @@ function ProductSliderLinkButtonV3({ data, pim }) {
 			) : (
 				<button
 					disabled
-					className={`sizes_item ${
+					className={`sizes_item ${theme === 'light' ? 'white' : 'dark'} ${
 						pim?.custom_fields[2]?.value === data.title
 							? 'btn-primary-new text-white'
 							: 'btn-outline-dark'
