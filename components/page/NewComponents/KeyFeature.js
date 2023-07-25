db2740f9bf225ac3b5a20cc43470d0bd7a636f55
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-07-25 15:25:34 +0330

    done

--- components/page/NewComponents/KeyFeature.js
+++ components/page/NewComponents/KeyFeature.js
@@ -14,27 +14,34 @@ const KeyFeature = ({ data }) => {
 					{structure?.list?.value.map((item, index) => (
 						<div
 							key={index}
-							className={`item`}
+							className={`item ${
+								structure?.list?.value.length < 4 ? 'banner' : ''
+							} ${item?.hover?.value ? 'hover_active' : 'hover_active'}`}
 							style={{ width: 100 / structure?.list?.value.length + '%' }}>
-							<div className='image_wrapper'>
+							<div className={`image_wrapper `}>
 								<img
 									src={item?.image?.src}
 									alt={item?.image?.alt}
 									className='image'
 								/>
 							</div>
-							{structure?.list?.value.length > 2 ? (
-								<h5 className='title'>{item?.title?.value}</h5>
-							) : (
-								<h4 className='title'>{item?.title?.value}</h4>
-							)}
-							<p className='subtitle'>{item?.subtitle?.value}</p>
 							<div
-								className='description'
-								dangerouslySetInnerHTML={{
-									__html: item?.description?.value
-								}}></div>
-							<p className='disclaimer'>{item?.subtitle?.value}</p>
+								className={`content ${
+									item?.dvider?.value ? 'divider' : 'divider'
+								}`}>
+								{structure?.list?.value.length > 2 ? (
+									<h5 className='title small'>{item?.title?.value}</h5>
+								) : (
+									<h4 className='title large'>{item?.title?.value}</h4>
+								)}
+								<p className='subtitle'>{item?.subtitle?.value}dasdarwef</p>
+								<div
+									className='description'
+									dangerouslySetInnerHTML={{
+										__html: item?.description?.value
+									}}></div>
+								<p className='disclaimer'>{item?.subtitle?.value}asdas</p>
+							</div>
 						</div>
 					))}
 				</div>
