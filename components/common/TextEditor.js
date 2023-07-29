950eb49f769e96ecedc69345fe4ce10ebf4571f6
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-07-29 11:35:58 +0330

    fixed

--- components/common/TextEditor.js
+++ components/common/TextEditor.js
@@ -7,15 +7,19 @@ function TextEditor({ data }) {
 		setText(structure?.text?.value ? structure?.text?.value : structure?.text)
 	}, [])
 	return (
-		<article
+		<section
 			style={{
-				maxWidth: `${structure?.width?.value}px`,
 				backgroundColor: structure?.backgroundColor?.value
-			}}
-			className={'article text-editor text_editor mx-auto pt-5 px-6 pb-6'}
-			dangerouslySetInnerHTML={{
-				__html: text
-			}}></article>
+			}}>
+			<article
+				style={{
+					maxWidth: `${structure?.width?.value}px`
+				}}
+				className={`${structure?.theme?.value} article text-editor text_editor mx-auto pt-5 px-6 pb-6`}
+				dangerouslySetInnerHTML={{
+					__html: text
+				}}></article>
+		</section>
 	)
 }
 
