1d9f3b37125cdcbe9ba4224e0317a9e19b7faf24
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-08-05 13:09:41 +0330

    .

--- services/cxm.js
+++ services/cxm.js
@@ -29,7 +29,9 @@ export async function GetNewsApi(filters, count, exclude) {
 
 export async function GetBlogsByTagApi(tag) {
 	let response = await axios.get(
-		`${process.env.NEXT_PUBLIC_CXM_API_ROUTE}/getPosts?type=blog&tag=${tag}&brand_id=${process.env.NEXT_PUBLIC_BRAND_ID}`
+		`${process.env.NEXT_PUBLIC_CXM_API_ROUTE}/getPosts?type=blog${
+			tag && `&tag=${tag}`
+		}&brand_id=${process.env.NEXT_PUBLIC_BRAND_ID}`
 	)
 	return response
 }
