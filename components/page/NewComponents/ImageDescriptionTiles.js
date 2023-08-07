450691ae87d612285d3a8204180b2e2d514b19aa
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-08-07 15:06:17 +0330

    fixed

--- components/page/NewComponents/ImageDescriptionTiles.js
+++ components/page/NewComponents/ImageDescriptionTiles.js
@@ -29,7 +29,8 @@ const ImageDescriptionTiles = ({ data }) => {
 								className={`content ${
 									structure?.divider?.value ? 'divider' : ''
 								}`}>
-								{item?.smallImage?.value && (
+								{(item?.smallImage?.value?.image?.src ||
+									item?.smallImage?.value?.text?.value) && (
 									<div className='small_image'>
 										{item?.smallImage?.value?.image?.src && (
 											<img
