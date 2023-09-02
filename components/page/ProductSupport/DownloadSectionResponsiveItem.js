a623e63f534d6f3b6efce761198a1d577ba42623
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-09-02 15:55:21 +0330

    .

---
+++ components/page/ProductSupport/DownloadSectionResponsiveItem.js
@@ -0,0 +1,80 @@
+import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import AdobeAcrobat from 'components/icons/AdobeAcrobat'
+import DownloadIconV2 from 'components/icons/DownloadIconV2'
+import Link from 'next/link'
+import React from 'react'
+import { useRef } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+const DownloadSectionResponsiveItem = ({ data, windowSize }) => {
+	const [open, setOpen] = useState(false)
+	const accordionItem = useRef()
+	const accordionItemHeight = useRef()
+	const accordionButtonHeight = useRef()
+	useEffect(() => {
+		if (open) {
+			accordionItem.current.style.maxHeight =
+				accordionItemHeight.current.offsetHeight +
+				accordionButtonHeight.current.offsetHeight +
+				85 +
+				'px'
+		} else {
+			accordionItem.current.style.maxHeight =
+				accordionButtonHeight.current.offsetHeight + 30 + 'px'
+		}
+	}, [open, windowSize])
+	return (
+		<div className='table_row' ref={accordionItem}>
+			<div
+				className='file_title'
+				onClick={() => setOpen(prev => !prev)}
+				ref={accordionButtonHeight}>
+				{data.caption ? data.caption : data.title}
+				<FontAwesomeIcon icon={faChevronCircleRight} size='lg' color={'#000'} />
+			</div>
+			<div ref={accordionItemHeight}>
+				<div className='info'>
+					<div>
+						<div className='size_title'>Size</div>
+						<div className='size'>
+							{data.caption ? data.caption : data.title}
+						</div>
+					</div>
+					<div className='file_type'>
+						{data.type_id === 3 ? (
+							<>
+								<span>PDF</span>
+								<AdobeAcrobat color='#fff' />
+							</>
+						) : (
+							'-'
+						)}
+					</div>
+				</div>
+				<div>
+					{data.url ? (
+						data.caption === 'Installation Request' ? (
+							<Link
+								target='_self'
+								href={'/televisions/laser-tv/installation-request'}>
+								<a className='download_item'>
+									<span>Download Update</span>
+									<DownloadIconV2 color='#00AAA6' />
+								</a>
+							</Link>
+						) : (
+							<a href={data.url} download={true} className='download_item'>
+								<span>Download Update</span>
+								<DownloadIconV2 color='#00AAA6' />
+							</a>
+						)
+					) : null}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default DownloadSectionResponsiveItem
---
+++ components/page/ProductSupport/DownloadsSection.js
@@ -0,0 +1,111 @@
+import AdobeAcrobat from 'components/icons/AdobeAcrobat'
+import DownloadIconV2 from 'components/icons/DownloadIconV2'
+import { useWindowSize } from 'hooks/useWindowSize'
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import DownloadSectionResponsiveItem from './DownloadSectionResponsiveItem'
+
+const DownloadsSection = ({ data, pim }) => {
+	let { structure } = data
+	const [content, setContent] = useState(null)
+	const windowSize = useWindowSize()
+	useEffect(() => {
+		setContent(structure)
+	}, [])
+	return (
+		<section>
+			<div className='downloads_section'>
+				<div className='text_box'>
+					<h4
+						className='title'
+						dangerouslySetInnerHTML={{ __html: content?.title?.value }}></h4>
+					<div
+						className='subtitle'
+						dangerouslySetInnerHTML={{
+							__html: content?.subtitle?.value
+						}}></div>
+					<div
+						className='description'
+						dangerouslySetInnerHTML={{
+							__html: content?.description?.value
+						}}></div>
+				</div>
+				<div className='table_wrapper'>
+					<div className='files_table'>
+						{windowSize[0] >= 768 ? (
+							<>
+								<div className='table_row'>
+									<div>Document</div>
+									<div>Size</div>
+									<div>File Type</div>
+									<div>Downlaod</div>
+								</div>
+								{pim?.assets.map(
+									(item, index) =>
+										item.type_id === 3 && (
+											<div key={index} className='table_row'>
+												<div className='file_title'>
+													{item.caption ? item.caption : item.title}
+												</div>
+												<div className='size'>
+													{item.caption ? item.caption : item.title}
+												</div>
+												<div className='file_type'>
+													{item.type_id === 3 ? (
+														<>
+															<span>PDF</span>
+															<AdobeAcrobat color='#fff' />
+														</>
+													) : (
+														'-'
+													)}
+												</div>
+												<div>
+													{item.url ? (
+														item.caption === 'Installation Request' ? (
+															<Link
+																target='_self'
+																href={
+																	'/televisions/laser-tv/installation-request'
+																}>
+																<a>
+																	<DownloadIconV2 color='#00AAA6' />
+																</a>
+															</Link>
+														) : (
+															<a href={item.url} download={true}>
+																<DownloadIconV2 color='#00AAA6' />
+															</a>
+														)
+													) : null}
+												</div>
+											</div>
+										)
+								)}
+							</>
+						) : (
+							<>
+								<div className='table_row'>
+									<div>Name</div>
+								</div>
+								{pim?.assets.map(
+									(item, index) =>
+										item.type_id === 3 && (
+											<DownloadSectionResponsiveItem
+												key={index}
+												data={item}
+												windowSize={windowSize}
+											/>
+										)
+								)}
+							</>
+						)}
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default DownloadsSection
