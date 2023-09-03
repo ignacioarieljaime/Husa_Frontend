4f9418dbcd03a6d474eed4646abcdcff999a1944
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-09-03 12:16:37 +0330

    updated compoenents

--- components/page/Product/ProductTextWithIconItem.js
+++ components/page/Product/ProductTextWithIconItem.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 const ProductTextWithIconItem = ({ image, title, description, isRow }) => {
 	return (
-		<div className={`col-12 ${isRow ? 'col-md-6' : ''} feature-block feature_block_item article`}>
-			<div className='mb-3'>
-				<img src={image.src} alt={image.alt} width='45' />
-			</div>
-			<h3>{title}</h3>
-			<p>{description}</p>
+		<div className={`col-12 ${isRow ? 'col-md-6' : ''} text_content`}>
+			<img src={image.src} alt={image.alt} className='image' />
+			<h3 className='title'>{title}</h3>
+			<p className='text'>{description}</p>
 		</div>
 	)
 }
