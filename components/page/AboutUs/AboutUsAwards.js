abaf35e1101391dd0f5784a6ab040ddd0538772b
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-09-22 02:33:37 +0330

    .

--- components/page/AboutUs/AboutUsAwards.js
+++ components/page/AboutUs/AboutUsAwards.js
@@ -18,6 +18,27 @@ const AboutUsAwards = ({ data }) => {
 						className='text'
 						dangerouslySetInnerHTML={{ __html: content?.text?.value }}></div>
 				</div>
+				<div className='list'>
+					{content?.list?.value.map((item, index) => (
+						<div
+							className='item'
+							key={index}
+							style={{ backgroundImage: `url(${item?.image?.src})` }}>
+							<div className='item_content'>
+								<h5
+									className='title'
+									dangerouslySetInnerHTML={{
+										__html: item?.title?.value
+									}}></h5>
+								<div
+									className='text'
+									dangerouslySetInnerHTML={{
+										__html: item?.text?.value
+									}}></div>
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		</section>
 	)
