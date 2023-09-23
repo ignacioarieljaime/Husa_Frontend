e0a53cc236acc588f7fc13f5339040dc6aaaa105
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-09-23 12:01:40 +0330

    autoplay fixed

--- components/page/Careers/CareersVideoBlock.js
+++ components/page/Careers/CareersVideoBlock.js
@@ -4,6 +4,7 @@ import { useRef } from 'react'
 
 const CareersVideoBlock = ({ data }) => {
 	const [content, setContent] = useState(null)
+	const [iframe, setIframe] = useState('')
 	const videoRef = useRef()
 	const [playingStatus, setPlayingStatus] = useState(true)
 
@@ -13,6 +14,24 @@ const CareersVideoBlock = ({ data }) => {
 
 	useEffect(() => {
 		if (videoRef.current) videoRef.current.play()
+		if (content?.videoType?.value === 'iframe') {
+			if (content?.video?.value.includes('?')) {
+				if (content?.video?.value.includes('https://player.vimeo.com')) {
+					setIframe(
+						content?.video?.value.split('?')[0] +
+							'?' +
+							content?.video?.value.split('?')[1].split('&amp;')[0] +
+							'&autoplay=1&muted=1&loop=1'
+					)
+				} else {
+					setIframe(
+						content?.video?.value.split('?')[0] + '?autoplay=1&muted=1&loop=1'
+					)
+				}
+			} else {
+				setIframe(content?.video?.value + '?autoplay=1&muted=1&loop=1')
+			}
+		}
 	}, [content?.video?.value])
 
 	const playVideo = _condition => {
@@ -37,9 +56,7 @@ const CareersVideoBlock = ({ data }) => {
 						src={content?.video?.value}
 						onClick={() => playVideo(true)}></video>
 				) : (
-					<iframe
-						allow='autoplay; mute; loop'
-						src={content?.video?.value + '?autoplay=1&mute=1&loop=1'}></iframe>
+					<iframe allow='autoplay; mute; loop' src={iframe}></iframe>
 				)}
 				{content?.videoType?.value === 'link' && !playingStatus && (
 					<button onClick={() => playVideo(playingStatus)}>
