89d195dedd8be38126a85901bdb76bec86ae13e4
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-10-05 00:08:45 +0330

    .

--- components/page/Upgrade/SeasonUpgradeProductsCarousel.js
+++ components/page/Upgrade/SeasonUpgradeProductsCarousel.js
@@ -80,7 +80,6 @@ const SeasonUpgradeProductsCarousel = ({ data }) => {
 							/>
 						</SwiperSlide>
 					))}
-
 					<button
 						className={`swiper-button-next ${
 							content?.selected_products?.value.length > 3 ? 'd-flex' : 'd-none'
