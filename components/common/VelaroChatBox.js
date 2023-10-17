99e64b0bf8358ffb304d2e327ccf4f4857aaa41d
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-10-17 18:59:29 +0330

    Revert "Revert "Revert "Merge pull request #350 from Influence-Marketing/New-chat-script"""
    This reverts commit 32737b3d1e59d8af706219c7ab8ec843353a693c.

--- components/common/VelaroChatBox.js
+++ components/common/VelaroChatBox.js
@@ -1,38 +1,15 @@
-import Script from 'next/script'
+import Head from 'next/head'
 import React, { useState, useEffect } from 'react'
 
+
 export default function VelaroChatBox() {
+const customCss = '.velaro-custom-launcher-frame{visibility: unset !important}'
+
 	return (
 		<>
-			<Script id='chat-script' strategy='afterInteractive'>
-				{`function getSourceId() {
-						var sourceId = 'SID20230811P001'
-						return sourceId
-					}
-					;(function (w, d, s, u, i) {
-						w.CicadaChat = function (c) {
-							w.CicadaChat._.push(c)
-						}
-						w.CicadaChat._ = []
-						w.CicadaChat.url = u
-						var h = d.getElementsByTagName(s)[0],
-							j = d.createElement(s)
-						j.async = true
-						j.src =
-							'https://hisense.livechat.prod.cicadaspeech.net/livechat/livechat.min.js?_=' +
-							Math.random()
-						CicadaChat(function () {
-							this.setSourceId({ id: i })
-						})
-						h.parentNode.insertBefore(j, h)
-					})(
-						window,
-						document,
-						'script',
-						'https://hisense.livechat.prod.cicadaspeech.net/install',
-						getSourceId()
-					)`}
-			</Script>
+			<Head>
+				<style>{customCss}</style>
+			</Head>
 		</>
 	)
 }
