ec17774d62fae62c65d82ceeec96a14973c6b3dd
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-11-07 09:05:54 -0500

    filter placement fix

--- components/page/ProductsV2/responsiveFilter/ProductFilterResponsive.js
+++ components/page/ProductsV2/responsiveFilter/ProductFilterResponsive.js
@@ -98,30 +98,26 @@ const ProductFilterResponsive = ({
 		filterRequest([], '')
 	}
 
-	// console.log("SECTION FINDER")
-	// console.log(document.querySelector(".product_filter_responsive_wrapper")?.parentNode?.parentElement.classList.add("overscroll-y-auto"));
-	// console.log(document.querySelector("section").closest(".product_filter_responsive_wrapper"))
-	/////////////
 	useEffect(() => {
-		const parentSection = document.querySelector(".product_filter_responsive_wrapper")?.parentNode?.parentNode;
 		if (modalIsOpen) {
+			document.querySelector(".product_filter_responsive_wrapper")?.scrollIntoView();
 			document.body.classList.add("overflow-y-clip");
 			document.querySelector(".product_filter_responsive_wrapper")?.parentNode?.parentElement.classList.add("overscroll-y-auto");
 			document.querySelector(".product_filter_responsive_wrapper")?.parentNode?.parentElement.classList.add("overflow-y-clip");
-			document.querySelector('.product_filter_responsive_wrapper').classList.add("overflow-y-clip");
+			document.querySelector('.product_filter_responsive_wrapper')?.classList.add("overflow-y-clip");
 		} else {
 
 			document.body.classList.remove("overflow-y-clip");
 			document.querySelector(".product_filter_responsive_wrapper")?.parentNode?.parentElement.classList.remove("overscroll-y-auto");
 			document.querySelector(".product_filter_responsive_wrapper")?.parentNode?.parentElement.classList.remove("overflow-y-clip");
-			document.querySelector('.product_filter_responsive_wrapper').classList.remove("overflow-y-clip");
+			document.querySelector('.product_filter_responsive_wrapper')?.classList.remove("overflow-y-clip");
 		}
 	
 		return () => {
 			document.body.classList.remove("overflow-y-clip");
 			document.querySelector(".product_filter_responsive_wrapper")?.parentNode?.parentElement.classList.remove("overscroll-y-auto");
 			document.querySelector(".product_filter_responsive_wrapper")?.parentNode?.parentElement.classList.remove("overflow-y-clip");
-			document.querySelector('.product_filter_responsive_wrapper').classList.remove("overflow-y-clip");
+			document.querySelector('.product_filter_responsive_wrapper')?.classList.remove("overflow-y-clip");
 		};
 	}, [modalIsOpen]);
 
