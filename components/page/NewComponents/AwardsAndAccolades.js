ae8d769c44a09c6a25eee96f74b3f926ddd78968
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-11-09 12:14:56 +0330

    finished pdp component

--- components/page/NewComponents/AwardsAndAccolades.js
+++ components/page/NewComponents/AwardsAndAccolades.js
@@ -1,11 +1,20 @@
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import AwardsNavIcon from 'components/icons/AwardsNavIcon'
+import { useWindowSize } from 'hooks/useWindowSize'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React, { useEffect, useRef } from 'react'
-import Swiper from 'swiper'
+import React, { useEffect, useRef, useState } from 'react'
+import { Navigation } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-export default function AwardsAndAccolades({ data, pim }) {
+function AwardsAndAccolades({ data, pim }) {
 	const ref = useRef(null)
 	const router = useRouter()
+	const windowSize = useWindowSize()
 	const [content, setContent] = useState({})
+	const navigationPrevRef = useRef(null)
+	const navigationNextRef = useRef(null)
 	useEffect(() => {
 		setContent(data?.structure)
 		if (router.asPath.includes(data?.name + data?.id)) {
@@ -24,32 +33,116 @@ export default function AwardsAndAccolades({ data, pim }) {
 				<h3
 					className='awards_and_accolades__title'
 					dangerouslySetInnerHTML={{ __html: content?.title?.value }}></h3>
-				{content?.list?.value && content?.list?.value.length <= 4 ? (
+				{(content?.list?.value && content?.list?.value.length === 1) ||
+				(content?.list?.value.length <= 4 && windowSize[0] > 768) ? (
 					<div className='awards_and_accolades__cards_wrapper'>
 						{content?.list?.value.map((item, index) => (
 							<div
-								className='awards_and_accolades__cards_wrapper__card'
+								className={`awards_and_accolades__cards_wrapper__card item__${
+									content?.list?.value.length <= 2
+										? content?.list?.value.length
+										: 3
+								}`}
 								key={index}>
-								<div className='awards_and_accolades__cards_wrapper__card__image'>
-									<img src={item?.image?.value} alt={item?.title?.value} />
-								</div>
+								<img
+									src={item?.image?.src}
+									alt={item?.title?.value}
+									className='awards_and_accolades__cards_wrapper__card__image'
+								/>
 								<div className='awards_and_accolades__cards_wrapper__card__content'>
-									<h4
-										className='awards_and_accolades__cards_wrapper__card__title'
-										dangerouslySetInnerHTML={{
-											__html: item?.title?.value
-										}}></h4>
-									<p
-										className='awards_and_accolades__cards_wrapper__card__text'
-										dangerouslySetInnerHTML={{ __html: item?.text?.value }}></p>
+									<div className='awards_and_accolades__cards_wrapper__card__content__texts'>
+										<h4
+											className='awards_and_accolades__cards_wrapper__card__content__texts__title'
+											dangerouslySetInnerHTML={{
+												__html: item?.title?.value
+											}}></h4>
+										<h6
+											className='awards_and_accolades__cards_wrapper__card__content__texts__subtilte'
+											dangerouslySetInnerHTML={{
+												__html: item?.subtitle?.value
+											}}></h6>
+									</div>
+									{item?.link?.value && (
+										<Link
+											href={item?.link?.value}
+											target={item?.link?.target ? item.link?.target : '_self'}>
+											<a
+												className='n-btn small primary-text awards_and_accolades__cards_wrapper__card__content__link'
+												target={
+													item?.link?.target ? item.link?.target : '_self'
+												}>
+												{item?.link?.title}
+												<FontAwesomeIcon icon={faChevronRight} />
+											</a>
+										</Link>
+									)}
 								</div>
 							</div>
 						))}
 					</div>
 				) : (
-					<Swiper></Swiper>
+					<Swiper
+						navigation={{
+							enabled: true,
+							prevEl: navigationPrevRef.current,
+							nextEl: navigationNextRef.current
+						}}
+						slidesPerView={'auto'}
+						grabCursor={true}
+						spaceBetween={windowSize[0] < 768 ? 16 : 40}
+						centeredSlides={windowSize[0] < 768 ? false : true}
+						modules={[Navigation]}
+						className='awards_and_accolades__cards_wrapper'>
+						{content?.list?.value.map((item, index) => (
+							<SwiperSlide
+								className={`awards_and_accolades__cards_wrapper__card item__3`}
+								key={index}>
+								<img
+									src={item?.image?.src}
+									alt={item?.title?.value}
+									className='awards_and_accolades__cards_wrapper__card__image'
+								/>
+								<div className='awards_and_accolades__cards_wrapper__card__content'>
+									<div className='awards_and_accolades__cards_wrapper__card__content__texts'>
+										<h4
+											className='awards_and_accolades__cards_wrapper__card__content__texts__title'
+											dangerouslySetInnerHTML={{
+												__html: item?.title?.value
+											}}></h4>
+										<h6
+											className='awards_and_accolades__cards_wrapper__card__content__texts__subtilte'
+											dangerouslySetInnerHTML={{
+												__html: item?.subtitle?.value
+											}}></h6>
+									</div>
+									{item?.link?.value && (
+										<Link
+											href={item?.link?.value}
+											target={item?.link?.target ? item.link?.target : '_self'}>
+											<a
+												className='n-btn small primary-text awards_and_accolades__cards_wrapper__card__content__link'
+												target={
+													item?.link?.target ? item.link?.target : '_self'
+												}>
+												{item?.link?.title}
+												<FontAwesomeIcon icon={faChevronRight} />
+											</a>
+										</Link>
+									)}
+								</div>
+							</SwiperSlide>
+						))}
+						<div className='swiper-button-prev' ref={navigationPrevRef}>
+							<AwardsNavIcon />
+						</div>
+						<div className='swiper-button-next' ref={navigationNextRef}>
+							<AwardsNavIcon />
+						</div>
+					</Swiper>
 				)}
 			</div>
 		</section>
 	)
 }
+
+export default AwardsAndAccolades
