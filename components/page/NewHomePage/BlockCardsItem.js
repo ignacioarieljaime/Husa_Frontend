0914aa6f2950c8e235976322a15fbdc2657bc32a
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-11-16 11:43:08 -0500

    Info cards fully hoverable and clickable, home page, televisions page, all dishwashers page

--- components/page/NewHomePage/BlockCardsItem.js
+++ components/page/NewHomePage/BlockCardsItem.js
@@ -6,62 +6,66 @@ import OpenPageOnNewTab from 'public/assets/images/OpenNewPageIcon.png'
 const HomePageMBTextedBoxesItem = ({ data, itemCount, index }) => {
 	let { backgroundImage, bottomImage, title, topImage, link } = data
 	return (
-		<div
-			className={`box flex-column justify-content-between d-flex  ${
-				itemCount % 2 !== 0 && index + 1 === itemCount ? 'full-size' : ''
-			}`}>
-			<div className='box-background'>
-				<CustomImage
-					src={backgroundImage?.src}
-					alt={backgroundImage?.alt}
-					wrapperHeight='100%'
-					wrapperWidth='100%'
-				/>
-			</div>
-			<div className='box-content'>
-				<div></div>
-
-				<div className='w-100'>
-					{topImage?.src && (
+		<Link
+			target={link?.target ? link?.target : '_self'}
+			href={link?.value ? link?.value : '/'}>
+			<a className={`box box-group-link flex-column justify-content-between d-flex  ${itemCount % 2 !== 0 && index + 1 === itemCount ? 'full-size' : ''
+				}`}>
+				<div>
+					<div className='box-background'>
 						<CustomImage
-							src={topImage?.src}
-							alt={topImage?.alt}
-							width='100%'
-							wrapperWidth={'100%'}
-							WrapperMaxWidth={'190px'}
-							wrapperClass='mb-4 top_image'
+							src={backgroundImage?.src}
+							alt={backgroundImage?.alt}
+							wrapperHeight='100%'
+							wrapperWidth='100%'
 						/>
-					)}
-					{bottomImage?.src && (
-						<img
-							src={bottomImage?.src}
-							alt={bottomImage?.alt}
-							width='100%'
-							className='mb-6'
-						/>
-					)}
+					</div>
+					<div className='box-content'>
+						<div></div>
 
-					<h4 className='mb-3 mb-md-6 box_title'>{title?.value}</h4>
-					{link?.title && link?.value ? (
-						<Link
-							target={link?.target ? link?.target : '_self'}
-							href={link?.value ? link?.value : '/'}>
-							<a
-								target={link?.target ? link?.target : '_self'}
-								className='n-btn outline-white transparent d-block w-fit medium'>
-								{link?.title}
-								{link?.target === '_blank' && (
-									<img
-										style={{ marginLeft: '10px' }}
-										src={OpenPageOnNewTab.src}
-									/>
-								)}
-							</a>
-						</Link>
-					) : null}
+						<div className='w-100'>
+							{topImage?.src && (
+								<CustomImage
+									src={topImage?.src}
+									alt={topImage?.alt}
+									width='100%'
+									wrapperWidth={'100%'}
+									WrapperMaxWidth={'190px'}
+									wrapperClass='mb-4 top_image'
+								/>
+							)}
+							{bottomImage?.src && (
+								<img
+									src={bottomImage?.src}
+									alt={bottomImage?.alt}
+									width='100%'
+									className='mb-6'
+								/>
+							)}
+
+							<h4 className='mb-3 mb-md-6 box_title'>{title?.value}</h4>
+							{link?.title && link?.value ? (
+								<Link
+									target={link?.target ? link?.target : '_self'}
+									href={link?.value ? link?.value : '/'}>
+									<a
+										target={link?.target ? link?.target : '_self'}
+										className='n-btn outline-white transparent d-block w-fit medium'>
+										{link?.title}
+										{link?.target === '_blank' && (
+											<img
+												style={{ marginLeft: '10px' }}
+												src={OpenPageOnNewTab.src}
+											/>
+										)}
+									</a>
+								</Link>
+							) : null}
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			</a>
+		</Link>
 	)
 }
 
