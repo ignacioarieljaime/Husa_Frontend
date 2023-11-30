b8b36973eafdd65b7b1544a3227efa5be1f71192
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-11-30 18:37:54 +0330

    added light box and cards qa

--- components/page/NewHomePage/BlockCardsItem.js
+++ components/page/NewHomePage/BlockCardsItem.js
@@ -1,72 +1,195 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import CustomImage from '../../common/CustomImage'
 import OpenPageOnNewTab from 'public/assets/images/OpenNewPageIcon.png'
+import LightBoxModal from '../NewComponents/LightBoxModal'
 
-const HomePageMBTextedBoxesItem = ({ data, itemCount, index }) => {
-	let { backgroundImage, bottomImage, title, topImage, link } = data
-	return (
-		<Link
-			target={link?.target ? link?.target : '_self'}
-			href={link?.value ? link?.value : '/'}>
-			<a className={`box box-group-link flex-column justify-content-between d-flex  ${itemCount % 2 !== 0 && index + 1 === itemCount ? 'full-size' : ''
+const BlockCardsItem = ({ data, itemCount, index }) => {
+	let {
+		backgroundImage,
+		bottomImage,
+		title,
+		topImage,
+		link,
+		gradient,
+		lightbox
+	} = data
+
+	const [lightBoxStatus, setLightBoxStatus] = useState(false)
+
+	const content = (
+		<div>
+			<div
+				className={`box-background ${
+					typeof gradient === 'undefined'
+						? 'gradient'
+						: gradient
+						? 'gradient'
+						: ''
 				}`}>
-				<div>
-					<div className='box-background'>
+				<CustomImage
+					src={backgroundImage?.src}
+					alt={backgroundImage?.alt}
+					wrapperHeight='100%'
+					wrapperWidth='100%'
+				/>
+			</div>
+			<div className='box-content'>
+				<div></div>
+
+				<div className='w-100'>
+					{topImage?.src && (
 						<CustomImage
-							src={backgroundImage?.src}
-							alt={backgroundImage?.alt}
-							wrapperHeight='100%'
-							wrapperWidth='100%'
+							src={topImage?.src}
+							alt={topImage?.alt}
+							width='100%'
+							wrapperWidth={'100%'}
+							WrapperMaxWidth={'190px'}
+							wrapperClass='mb-4 top_image'
 						/>
-					</div>
-					<div className='box-content'>
-						<div></div>
+					)}
+					{bottomImage?.src && (
+						<img
+							src={bottomImage?.src}
+							alt={bottomImage?.alt}
+							width='100%'
+							className='mb-6'
+						/>
+					)}
 
-						<div className='w-100'>
-							{topImage?.src && (
-								<CustomImage
-									src={topImage?.src}
-									alt={topImage?.alt}
-									width='100%'
-									wrapperWidth={'100%'}
-									WrapperMaxWidth={'190px'}
-									wrapperClass='mb-4 top_image'
-								/>
-							)}
-							{bottomImage?.src && (
-								<img
-									src={bottomImage?.src}
-									alt={bottomImage?.alt}
-									width='100%'
-									className='mb-6'
-								/>
-							)}
-
-							<h4 className='mb-3 mb-md-6 box_title'>{title?.value}</h4>
-							{link?.title && link?.value ? (
-								<Link
+					<h4 className='mb-3 mb-md-6 box_title'>{title?.value}</h4>
+					{link?.title ? (
+						lightbox?.value ? (
+							<button
+								onClick={() => setLightBoxStatus(true)}
+								className='n-btn outline-white transparent d-block w-fit medium'>
+								{link?.title}
+							</button>
+						) : link?.value ? (
+							<Link
+								target={link?.target ? link?.target : '_self'}
+								href={link?.value ? link?.value : '/'}>
+								<a
 									target={link?.target ? link?.target : '_self'}
-									href={link?.value ? link?.value : '/'}>
-									<a
-										target={link?.target ? link?.target : '_self'}
-										className='n-btn outline-white transparent d-block w-fit medium'>
-										{link?.title}
-										{link?.target === '_blank' && (
-											<img
-												style={{ marginLeft: '10px' }}
-												src={OpenPageOnNewTab.src}
-											/>
-										)}
-									</a>
-								</Link>
-							) : null}
+									className='n-btn outline-white transparent d-block w-fit medium'>
+									{link?.title}
+									{link?.target === '_blank' && (
+										<img
+											style={{ marginLeft: '10px' }}
+											src={OpenPageOnNewTab.src}
+										/>
+									)}
+								</a>
+							</Link>
+						) : null
+					) : null}
+				</div>
+			</div>
+		</div>
+	)
+
+	return (
+		<>
+			{link?.value ? (
+				<Link
+					target={link?.target ? link?.target : '_self'}
+					href={link?.value ? link?.value : '/'}>
+					<a
+						className={`box box-group-link flex-column justify-content-between d-flex  ${
+							itemCount % 2 !== 0 && index + 1 === itemCount ? 'full-size' : ''
+						}`}>
+						{content}
+					</a>
+				</Link>
+			) : (
+				<div
+					onClick={() => setLightBoxStatus(prevState => !prevState)}
+					className={`box box-group-link flex-column justify-content-between d-flex  ${
+						itemCount % 2 !== 0 && index + 1 === itemCount ? 'full-size' : ''
+					}`}>
+					<div>
+						<div
+							className={`box-background ${
+								typeof gradient === 'undefined'
+									? 'gradient'
+									: gradient
+									? 'gradient'
+									: ''
+							}`}>
+							<CustomImage
+								src={backgroundImage?.src}
+								alt={backgroundImage?.alt}
+								wrapperHeight='100%'
+								wrapperWidth='100%'
+							/>
+						</div>
+						<div className='box-content'>
+							<div></div>
+
+							<div className='w-100'>
+								{topImage?.src && (
+									<CustomImage
+										src={topImage?.src}
+										alt={topImage?.alt}
+										width='100%'
+										wrapperWidth={'100%'}
+										WrapperMaxWidth={'190px'}
+										wrapperClass='mb-4 top_image'
+									/>
+								)}
+								{bottomImage?.src && (
+									<img
+										src={bottomImage?.src}
+										alt={bottomImage?.alt}
+										width='100%'
+										className='mb-6'
+									/>
+								)}
+
+								<h4 className='mb-3 mb-md-6 box_title'>{title?.value}</h4>
+								{link?.title ? (
+									lightbox?.value ? (
+										<button
+											onClick={() => setLightBoxStatus(true)}
+											className='n-btn outline-white transparent d-block w-fit medium'>
+											{link?.title}
+										</button>
+									) : link?.value ? (
+										<Link
+											target={link?.target ? link?.target : '_self'}
+											href={link?.value ? link?.value : '/'}>
+											<a
+												target={link?.target ? link?.target : '_self'}
+												className='n-btn outline-white transparent d-block w-fit medium'>
+												{link?.title}
+												{link?.target === '_blank' && (
+													<img
+														style={{ marginLeft: '10px' }}
+														src={OpenPageOnNewTab.src}
+													/>
+												)}
+											</a>
+										</Link>
+									) : null
+								) : null}
+							</div>
 						</div>
 					</div>
 				</div>
-			</a>
-		</Link>
+			)}
+			{lightbox?.value && (
+				<LightBoxModal
+					id={data?.id}
+					caption={lightbox?.value?.caption}
+					video={lightbox?.value?.video}
+					image={lightbox?.value?.image}
+					link={lightbox?.value?.link}
+					isVisible={lightBoxStatus}
+					visibleHandler={() => setLightBoxStatus(prevState => !prevState)}
+				/>
+			)}
+		</>
 	)
 }
 
-export default HomePageMBTextedBoxesItem
+export default BlockCardsItem
