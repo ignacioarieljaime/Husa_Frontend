edeca815deadad7457751d1d9ee3ee6513607438
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-12-05 13:01:21 -0500

    x replaced with search icon onBlur

--- components/page/NewsRoom/NewsSearchFilter.js
+++ components/page/NewsRoom/NewsSearchFilter.js
@@ -188,7 +188,10 @@ const NewsSearchFilter = ({
 												if (e.key === 'Enter' && !results)
 													redirectToResultsPage()
 											}}
-											onBlur={() => !results && redirectToResultsPage()}
+											onBlur={() => {
+												setSearchFocus(prev => !prev)
+												!results && redirectToResultsPage()
+											}}
 											onFocus={() => setSearchFocus(prev => !prev)}
 										/>
 										{searchFocus ?
