4a9b156ef1313b1ca79ba687d6ffa6cd50fc9e36
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-12-13 19:24:35 +0330

    new component added

--- utils/components/NewsRoomComponents.js
+++ utils/components/NewsRoomComponents.js
@@ -1,42 +1,46 @@
 module.exports.NewsRoomComponents = [
 	{
-		name: 'NewsRoomSlider ',
+		name: 'NewsRoomSlider',
 		path: 'components/page/NewsRoom/NewsRoomSlider'
 	},
 	{
-		name: 'NewsRoomNewsBox ',
+		name: 'NewsRoomNewsBox',
 		path: 'components/page/NewsRoom/NewsRoomNewsBox'
 	},
 	{
-		name: 'NewsRoomMedia ',
+		name: 'NewsRoomMedia',
 		path: 'components/page/NewsRoom/NewsRoomMedia'
 	},
 	{
-		name: 'NewsIntro ',
+		name: 'NewsIntro',
 		path: 'components/page/NewsRoom/SingleNews/NewsIntro'
 	},
 	{
-		name: 'NewsSingleSlider ',
+		name: 'NewsSingleSlider',
 		path: 'components/page/NewsRoom/SingleNews/NewsSingleSlider'
 	},
 	{
-		name: 'NewsSingleDescription ',
+		name: 'NewsSingleDescription',
 		path: 'components/page/NewsRoom/SingleNews/NewsSingleDescription'
 	},
 	{
-		name: 'NewsSingleInfoBox ',
+		name: 'NewsSingleInfoBox',
 		path: 'components/page/NewsRoom/SingleNews/NewsSingleInfoBox'
 	},
 	{
-		name: 'NewsSingleLatestNews ',
+		name: 'NewsSingleLatestNews',
 		path: 'components/page/NewsRoom/SingleNews/NewsSingleLatestNews'
 	},
 	{
-		name: 'NewsPressArchive ',
+		name: 'NewsPressArchive',
 		path: 'components/page/NewsRoom/SingleNews/NewsPressArchive'
 	},
 	{
-		name: 'NewsRoomMainBox ',
+		name: 'NewsRoomMainBox',
 		path: 'components/page/NewsRoom/NewsRoomMainBox'
+	},
+	{
+		name: 'NewsRoomAnnouncementsForm',
+		path: 'components/page/NewsRoom/NewsRoomAnnouncementsForm'
 	}
 ]
