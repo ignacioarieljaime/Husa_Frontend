c131874df251d22707225b5aaba350f1b1cd5b38
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-12-19 15:28:59 -0500

    merge fix 2

--- components/page/NewComponents/LightBoxModal.js
+++ components/page/NewComponents/LightBoxModal.js
@@ -215,6 +215,43 @@ const LightBoxModal = ({
 							<FontAwesomeIcon icon={faChevronLeft} />
 							<span>Back</span>
 						</button>
+						{/* {!dataList ?
+							<Link
+								target={link?.target ? link?.target : '_self'}
+								href={
+									link?.value ?
+									link?.value :
+									image?.src ?
+									image?.src
+									: '#'
+								}
+								>
+								<a
+									target={link?.target ? link?.target : '_self'}
+									className='n-btn outline-black transparent d-flex gap-2 align-items-center w-fit medium'>
+									{link?.title}
+									<DownloadIconV2 color='#000' width='16' height='16' />
+								</a>
+							</Link> :
+							(dataList[currentIndex]?.link?.value || dataList[currentIndex]?.image?.src) && dataList[currentIndex]?.link?.title && (
+								<Link
+									target={dataList[currentIndex]?.link?.target ? dataList[currentIndex]?.link?.target : '_self'}
+									href={
+										dataList[currentIndex]?.link?.value ?
+										dataList[currentIndex]?.link?.value :
+										dataList[currentIndex]?.image?.src ?
+										dataList[currentIndex]?.image?.src
+										: '#'
+									}
+									>
+									<a
+										target={link?.target ? link?.target : '_self'}
+										className='n-btn outline-black transparent d-flex gap-2 align-items-center w-fit medium'>
+										{link?.title}
+										<DownloadIconV2 color='#000' width='16' height='16' />
+									</a>
+								</Link>
+						)} */}
 						{!dataList ?
 							<Link
 								target={link?.target ? link?.target : '_self'}
