1fc994aba43524c87d0e599ae76884af60bae069
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2023-12-21 17:18:38 +0330

    added bamboo

--- components/common/BambooHR.js
+++ components/common/BambooHR.js
@@ -9,11 +9,19 @@ const BambooHR = () => {
 				data-domain='hisenseusacorporation.bamboohr.com'
 				data-version='1.0.0'
 				data-departmentId=''></div>
+<<<<<<< HEAD
 			<Script
 				src='https://hisenseusacorporation.bamboohr.com/js/embed.js'
 				type='text/javascript'
 				async
 				defer></Script>
+=======
+			<script
+				src='https://hisenseusacorporation.bamboohr.com/js/embed.js'
+				type='text/javascript'
+				async
+				defer></script>
+>>>>>>> 58fe5fc2 (added bamboo)
 		</>
 	)
 }
