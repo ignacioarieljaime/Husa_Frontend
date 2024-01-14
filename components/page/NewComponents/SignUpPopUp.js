e7e8a78ec38dab1b5a7f8957f518dffdb7475f71
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2024-01-14 12:49:55 +0330

    updated the form

--- components/page/NewComponents/SignUpPopUp.js
+++ components/page/NewComponents/SignUpPopUp.js
@@ -9,27 +9,18 @@ const SignUpPopUp = ({ data }) => {
 	const [hasClosed, setHasClosed] = useState(false)
 	const [loading, setLoading] = useState(false)
 	const [isOpen, setIsOpen] = useState(false)
-	const [formBody, dispatch] = useReducer(
-		(state, updatedState) => ({
-			...state,
-			...updatedState
-		}),
-		{
-			email: '',
-		}
-	)
+	const [email, setEmail] = useState('')
+
+	const timeoutNumber = Number(content?.timeout?.value * 1000)
+	const scrollNumber = Number(content?.distance?.value)
 
 	useEffect(() => {
 		setContent(structure)
 	}, [])
 
-	function formHandler(_key, _value) {
-		dispatch({ [_key]: _value })
-	}
-
 	async function submitHandler(e) {
 		e.preventDefault()
-		console.log({"EMAIL": formBody.email})
+		console.log({ EMAIL: email })
 		// NOTE FROM BEN:
 		// I dont know how our API works so this code will need to be modified by you
 		// The email needs to get added to the HiSense Lead list in the CXM
@@ -60,111 +51,102 @@ const SignUpPopUp = ({ data }) => {
 		// Closes modal
 		setHasClosed(true)
 		setIsOpen(false)
-
 	}
-
-
-	const timeoutNumber = Number((content?.timeout?.value) * 1000);
-	const scrollNumber = Number(content?.distance?.value);
 
 	const handleScroll = () => {
 		if (typeof window !== 'undefined' && typeof scrollNumber === 'number') {
-		  const newScrollY = Math.round(window.scrollY);
-	  
-		  if (newScrollY >= scrollNumber && !isOpen && !hasClosed) {
-			setIsOpen(true);
-		  }
+			const newScrollY = Math.round(window.scrollY)
+
+			if (newScrollY >= scrollNumber && !isOpen && !hasClosed) {
+				setIsOpen(true)
+			}
 		}
-	  };
-	
+	}
 
 	useEffect(() => {
-		window.addEventListener('scroll', handleScroll);
+		window.addEventListener('scroll', handleScroll)
 
 		return () => {
-			window.removeEventListener('scroll', handleScroll);
-		};
-	}, [scrollNumber]);
+			window.removeEventListener('scroll', handleScroll)
+		}
+	}, [scrollNumber])
 
 	if (timeoutNumber)
 		setTimeout(() => {
 			if (!hasClosed && !isOpen) setIsOpen(true)
-		}, timeoutNumber);
+		}, timeoutNumber)
 
 	const closeHandler = () => {
 		if (isOpen) setIsOpen(false)
 		if (!hasClosed) setHasClosed(true)
 	}
-	
 
-	if (isOpen && !hasClosed)
 	return (
-		<div className='signup_popup'>
-			<div className='signup_popup___container'>
-				<div className='signup_popup___container___content'>
-					<div className='signup_popup___container___content___image'>
-						<img
-							src={content?.image?.src}
-							alt={content?.image?.alt}
-							className='background'
-						/>
+		isOpen &&
+		!hasClosed && (
+			<div className='signup_popup'>
+				<div className='signup_popup___container'>
+					<div className='signup_popup___container___content'>
+						<div className='signup_popup___container___content___image'>
+							<img
+								src={content?.image?.src}
+								alt={content?.image?.alt}
+								className='background'
+							/>
+						</div>
+						<div className='signup_popup___container___content___text'>
+							<h5 className='signup_popup___container___content___text___title'>
+								{content?.title?.value}
+							</h5>
+
+							<form
+								className='signup_popup___container___content___text___form'
+								onSubmit={submitHandler}>
+								<div className='signup_popup___container___content___text___form___field'>
+									<input
+										name='email'
+										type='email'
+										placeholder='Email Address'
+										required
+										value={email}
+										onChange={e => setEmail(e.target.value)}
+									/>
+								</div>
+
+								<div className='signup_popup___container___content___text___form___buttons'>
+									<button
+										type='submit'
+										disabled={loading}
+										className='signup_popup___container___content___text___form___buttons___submit_btn'>
+										{loading ? (
+											<Spinner color='#000' size='sm' />
+										) : (
+											<span>Subscribe</span>
+										)}
+									</button>
+
+									<button
+										button='button'
+										className='signup_popup___container___content___text___form___buttons___cancel_btn'
+										onClick={() => closeHandler()}>
+										<span>No Thanks</span>
+									</button>
+								</div>
+							</form>
+						</div>
 					</div>
-					<div className='signup_popup___container___content___text'>
 
-						<h5 className='signup_popup___container___content___text___title'>
-							{content?.title?.value}
-						</h5>
-
-						<form className='signup_popup___container___content___text___form'
-							onSubmit={submitHandler}
-						>
-
-							<div className='signup_popup___container___content___text___form___field'>
-								<input
-									name='email'
-									type='email'
-									placeholder='Email Address'
-									required
-									value={formBody.email}
-									onChange={e => formHandler(e.target.name, e.target.value)}
-								/>
-							</div>
-
-							<div className='signup_popup___container___content___text___form___buttons'>
-								<button
-									disabled={loading}
-									className='signup_popup___container___content___text___form___buttons___submit_btn'>
-									{loading ? (
-										<Spinner color='#000' size='sm' />
-									) : (
-										<span>Subscribe</span>
-									)}
-								</button>
-
-								<button className='signup_popup___container___content___text___form___buttons___cancel_btn'
-									onClick={() => closeHandler()}
-								>
-									<span>No Thanks</span>
-								</button>
-							</div>
-
-						</form>
-					</div>
+					<button
+						className='signup_popup___container___close_btn'
+						onClick={() => closeHandler()}>
+						<span>
+							<XIcon />
+						</span>
+					</button>
 				</div>
-
-				<button className='signup_popup___container___close_btn'
-					onClick={() => closeHandler()}
-				>
-					<span>
-						<XIcon />
-					</span>
-				</button>
-
 			</div>
-		</div>
+		)
 	)
-
-	return null;
 }
 
 export default SignUpPopUp
