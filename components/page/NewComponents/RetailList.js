382207b6436510e2da0d3c65402b0911ab7ab945
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2024-01-20 16:23:32 +0330

    updated the component

---
+++ components/page/NewComponents/RetailList.js
@@ -0,0 +1,302 @@
+import React from 'react'
+
+const RetailList = ({ data }) => {
+	const { structure } = data
+	const list = {
+		value: [
+			{
+				title: {
+					value: '<p>Alaska</p>'
+				},
+				sublist: {
+					value: [
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						}
+					]
+				}
+			},
+			{
+				title: {
+					value: '<p>Alaska</p>'
+				},
+				sublist: {
+					value: [
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						}
+					]
+				}
+			},
+			{
+				title: {
+					value: '<p>Alaska</p>'
+				},
+				sublist: {
+					value: [
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						}
+					]
+				}
+			},
+			{
+				title: {
+					value: '<p>Alaska</p>'
+				},
+				sublist: {
+					value: [
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						}
+					]
+				}
+			},
+			{
+				title: {
+					value: '<p>Alaska</p>'
+				},
+				sublist: {
+					value: [
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						}
+					]
+				}
+			},
+			{
+				title: {
+					value: '<p>Alaska</p>'
+				},
+				sublist: {
+					value: [
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						}
+					]
+				}
+			},
+			{
+				title: {
+					value: '<p>Alaska</p>'
+				},
+				sublist: {
+					value: [
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						},
+						{
+							title: {
+								value: '<p>Best Buy</p>'
+							},
+							address: {
+								value: `<p>Dimond mall</p>              
+                                <p>800 E Dimond Blvd STE 100</p> 
+                                <p>Anchorage, AK 99515</p>`
+							}
+						}
+					]
+				}
+			}
+		]
+	}
+	return (
+		<section>
+			<div id={data?.name + data?.id} className='retail_list'>
+				<div className='retail_list___content'>
+					<h5
+						className='retail_list___content___title'
+						dangerouslySetInnerHTML={{ __html: structure.title.value }}></h5>
+					<ul className='retail_list___content___retailers'>
+						{list.value.map((item, index) =>
+							item.sublist.value.map((subitem, subindex) => (
+								<li
+									className='retail_list___content___retailers___item'
+									key={index * 10 + subindex}>
+									{subindex < 1 && (
+										<h5
+											className='retail_list___content___retailers___item___title'
+											dangerouslySetInnerHTML={{
+												__html: item.title.value
+											}}></h5>
+									)}
+									<div
+										className='retail_list___content___retailers___item___subtitem___title'
+										dangerouslySetInnerHTML={{
+											__html: subitem?.title?.value
+										}}></div>
+									<div
+										className='retail_list___content___retailers___item___subtitem___address'
+										dangerouslySetInnerHTML={{
+											__html: subitem?.address?.value
+										}}></div>
+								</li>
+							))
+						)}
+					</ul>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default RetailList
