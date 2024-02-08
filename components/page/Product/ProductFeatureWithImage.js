4e5dedc26cbaa2d6637b1c7dbda80adce855c7b2
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2024-02-08 16:00:41 -0500

    Revert "Adjust font as per Figma mockups"
    This reverts commit 57d2f5e36d6ae48b1091b8f48493862057c0235e.

--- components/page/Product/ProductFeatureWithImage.js
+++ components/page/Product/ProductFeatureWithImage.js
@@ -65,9 +65,7 @@ function ProductFeatureWithImage({ data }) {
 					<article className='introduction col-12 col-md-6 order-2'>
 						<h4
 							style={{
-								color: structure?.theme?.value === 'dark' ? '#fff' : '#000',
-								fontSize: '20px',
-								fontWeight: '500',
+								color: structure?.theme?.value === 'dark' ? '#fff' : '#000'
 							}}>
 							{structure?.littleTitle?.value}
 						</h4>
@@ -75,9 +73,8 @@ function ProductFeatureWithImage({ data }) {
 						<h4
 							style={{
 								lineHeight: 'unset',
-								fontSize: '48px',
-								color: structure?.theme?.value === 'dark' ? '#fff' : '#000',
-								fontWeight: '500',
+								fontSize: '42px',
+								color: structure?.theme?.value === 'dark' ? '#fff' : '#000'
 							}}>
 							{structure?.title?.value}
 						</h4>
