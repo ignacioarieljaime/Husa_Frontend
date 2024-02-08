7ad0e57d552626d147dc41a6fd3b57262f9933d6
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2024-02-08 16:00:37 -0500

    Revert "Make minor changes"
    This reverts commit 2bb30ad37c9e5c7dda71f3df91f526702be7e0d6.

--- components/page/Product/ProductFeatureWithImage.js
+++ components/page/Product/ProductFeatureWithImage.js
@@ -83,8 +83,7 @@ function ProductFeatureWithImage({ data }) {
 						</h4>
 						<p
 							style={{
-								color: structure?.theme?.value === 'dark' ? '#fff' : '#000',
-								fontWeight: 400,
+								color: structure?.theme?.value === 'dark' ? '#fff' : '#000'
 							}}>
 							{structure?.description?.value}
 						</p>
