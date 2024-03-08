80dd74f2f579232f5300db62d7b448f8191d0978
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2024-03-08 14:19:33 -0500

    bamboo hr conditional render from branch imd 1034

--- components/common/BambooHR.js
+++ components/common/BambooHR.js
@@ -1,21 +1,42 @@
 import Script from 'next/script'
 import React from 'react'
+import { useRouter } from 'next/router'
 
 const BambooHR = () => {
+
+	const router = useRouter()
+	const pathName = router?.pathname
+
+	if (pathName === '/careers/job-postings') {
+		return (
+			<>
+				<div
+					id='BambooHR'
+					data-domain='hisenseusacorporation.bamboohr.com'
+					data-version='1.0.0'
+					data-departmentId=''></div>
+				<Script
+					src='https://hisenseusacorporation.bamboohr.com/js/embed.js'
+					type='text/javascript'
+					async
+					defer></Script>
+			</>
+		)
+	}
 	return (
 		<>
 			<div
-				id='BambooHR'
-				data-domain='hisenseusacorporation.bamboohr.com'
-				data-version='1.0.0'
-				data-departmentId=''></div>
+				id='fancompass-iframe-container'
+				data-domain='fancompass.hisense-usa.com'
+				data-version='1.0.0'></div>
 			<Script
-				src='https://hisenseusacorporation.bamboohr.com/js/embed.js'
+				src='https://fancompass.hisense-usa.com/embed-script'
 				type='text/javascript'
 				async
 				defer></Script>
 		</>
 	)
+
 }
 
 export default BambooHR
