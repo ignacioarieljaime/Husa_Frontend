ad5a12d42d44bba4834a77dfadcf6e0b34459cf2
ignacioarieljaime <ignaciojaimewordcoin@gmail.com>
2024-05-07 10:34:28 -0400

    ignore this from now on

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,6 +4,7 @@ let robotAccessibility = process.env.ROBOT_ACCESSIBILITY || false
 console.log('robotAccessibility:', robotAccessibility);
 let siteUrl = process.env.SITE_URL || 'https://public.stage.hisenseportal.com/'
 
+
 module.exports = {
 	siteUrl: siteUrl,
 	generateRobotsTxt: true,
